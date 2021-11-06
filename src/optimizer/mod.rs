31b386e76365335e4d37277d636c193b7efb84e3
Mingj <mingji_han@outlook.com>
2021-11-06 15:04:28 -0400

    complete framework

--- src/optimizer/mod.rs
+++ src/optimizer/mod.rs
@@ -1,4 +1,8 @@
-use crate::logical_planner::LogicalPlan;
+use crate::logical_planner::{
+    LogicalCreateTable, LogicalDelete, LogicalDrop, LogicalExplain, LogicalFilter, LogicalInsert,
+    LogicalJoin, LogicalJoinTable, LogicalLimit, LogicalOrder, LogicalPlan, LogicalProjection,
+    LogicalSeqScan,
+};
 // The optimizer will do query optimization.
 // It will do both rule-based optimization (predicate pushdown, constant folding and common expression extraction)
 // , and cost-based optimization (Join reordering and join algorithm selection).
@@ -18,21 +22,86 @@ pub trait PlanRewriter {
     fn rewrite_plan(&mut self, plan: LogicalPlan) -> LogicalPlan {
         match plan {
             LogicalPlan::Dummy => LogicalPlan::Dummy,
-            LogicalPlan::CreateTable(plan) => LogicalPlan::CreateTable(plan),
-            LogicalPlan::Drop(plan) => LogicalPlan::Drop(plan),
-            LogicalPlan::Insert(plan) => LogicalPlan::Insert(plan),
-            LogicalPlan::Join(plan) => LogicalPlan::Join(plan),
-            LogicalPlan::SeqScan(plan) => LogicalPlan::SeqScan(plan),
-            LogicalPlan::Projection(plan) => LogicalPlan::Projection(plan),
-            LogicalPlan::Filter(plan) => LogicalPlan::Filter(plan),
-            LogicalPlan::Order(plan) => LogicalPlan::Order(plan),
-            LogicalPlan::Limit(plan) => LogicalPlan::Limit(plan),
-            LogicalPlan::Explain(plan) => LogicalPlan::Explain(plan),
-            LogicalPlan::Delete(plan) => LogicalPlan::Delete(plan),
+            LogicalPlan::CreateTable(plan) => self.rewrite_create_table(plan),
+            LogicalPlan::Drop(plan) => self.rewrite_drop(plan),
+            LogicalPlan::Insert(plan) => self.rewrite_insert(plan),
+            LogicalPlan::Join(plan) => self.rewrite_join(plan),
+            LogicalPlan::SeqScan(plan) => self.rewrite_seqscan(plan),
+            LogicalPlan::Projection(plan) => self.rewrite_projection(plan),
+            LogicalPlan::Filter(plan) => self.rewrite_filter(plan),
+            LogicalPlan::Order(plan) => self.rewrite_order(plan),
+            LogicalPlan::Limit(plan) => self.rewrite_limit(plan),
+            LogicalPlan::Explain(plan) => self.rewrite_explain(plan),
+            LogicalPlan::Delete(plan) => self.rewrite_delete(plan),
         }
     }
 
     fn rewrite_create_table(&mut self, plan: LogicalCreateTable) -> LogicalPlan {
-        
+        LogicalPlan::CreateTable(plan)
+    }
+
+    fn rewrite_drop(&mut self, plan: LogicalDrop) -> LogicalPlan {
+        LogicalPlan::Drop(plan)
+    }
+
+    fn rewrite_insert(&mut self, plan: LogicalInsert) -> LogicalPlan {
+        LogicalPlan::Insert(plan)
+    }
+
+    fn rewrite_join(&mut self, plan: LogicalJoin) -> LogicalPlan {
+        let mut join_table_plans = vec![];
+        for plan in plan.join_table_plans.into_iter() {
+            join_table_plans.push(LogicalJoinTable {
+                table_plan: Box::new(self.rewrite_plan(*plan.table_plan)),
+                join_op: plan.join_op,
+            });
+        }
+        LogicalPlan::Join(LogicalJoin {
+            relation_plan: Box::new(self.rewrite_plan(*plan.relation_plan)),
+            join_table_plans,
+        })
+    }
+
+    fn rewrite_seqscan(&mut self, plan: LogicalSeqScan) -> LogicalPlan {
+        LogicalPlan::SeqScan(plan)
+    }
+
+    fn rewrite_projection(&mut self, plan: LogicalProjection) -> LogicalPlan {
+        LogicalPlan::Projection(LogicalProjection {
+            project_expressions: plan.project_expressions,
+            child: Box::new(self.rewrite_plan(*plan.child)),
+        })
+    }
+
+    fn rewrite_filter(&mut self, plan: LogicalFilter) -> LogicalPlan {
+        LogicalPlan::Filter(LogicalFilter {
+            expr: plan.expr,
+            child: Box::new(self.rewrite_plan(*plan.child)),
+        })
+    }
+
+    fn rewrite_order(&mut self, plan: LogicalOrder) -> LogicalPlan {
+        LogicalPlan::Order(LogicalOrder {
+            comparators: plan.comparators,
+            child: Box::new(self.rewrite_plan(*plan.child)),
+        })
+    }
+
+    fn rewrite_limit(&mut self, plan: LogicalLimit) -> LogicalPlan {
+        LogicalPlan::Limit(LogicalLimit {
+            offset: plan.offset,
+            limit: plan.limit,
+            child: Box::new(self.rewrite_plan(*plan.child)),
+        })
+    }
+
+    fn rewrite_explain(&mut self, plan: LogicalExplain) -> LogicalPlan {
+        LogicalPlan::Explain(LogicalExplain {
+            plan: Box::new(self.rewrite_plan(*plan.plan)),
+        })
+    }
+
+    fn rewrite_delete(&mut self, plan: LogicalDelete) -> LogicalPlan {
+        LogicalPlan::Delete(plan)
     }
 }
