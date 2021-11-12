9a70502e22bc7a67735e45b66c6f26801e37c69b
Mingj <mingji_han@outlook.com>
2021-11-11 21:20:03 -0500

    Update projection.rs

--- src/physical_planner/projection.rs
+++ src/physical_planner/projection.rs
@@ -22,12 +22,7 @@ impl PhysicalPlaner {
 
 impl PlanExplainable for PhysicalProjection {
     fn explain_inner(&self, level: usize, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "Projection: {} exprs", self.project_expressions.len())?;
-        writeln!(f, "Exprs:")?;
-        for expr in self.project_expressions.iter() {
-            writeln!(f, "{:?}", expr)?;
-        }
-
+        writeln!(f, "Projection: exprs {:?}", self.project_expressions)?;
         self.child.explain(level + 1, f)
     }
 }
