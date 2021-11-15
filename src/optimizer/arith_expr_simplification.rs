23a0679efbcaf320cbd3a191df88c70214b64cfc
Runji Wang <wangrunji0408@163.com>
2021-11-15 20:44:59 +0800

    simplify arith_expr_simplification

--- src/optimizer/arith_expr_simplification.rs
+++ src/optimizer/arith_expr_simplification.rs
@@ -1,7 +1,7 @@
 use super::*;
-use crate::binder::{BoundExpr, BoundExprKind};
-use crate::parser::BinaryOperator;
-use crate::types::DataValue;
+use crate::binder::{BoundExpr, BoundExprKind::*};
+use crate::parser::{BinaryOperator::*, UnaryOperator};
+use crate::types::{DataTypeKind as Ty, DataValue::*};
 
 /// Arithemtic expression simplification rule prunes the useless constant in the binary expressions.
 ///
@@ -14,127 +14,49 @@ pub struct ArithExprSimplification;
 impl PlanRewriter for ArithExprSimplification {
     fn rewrite_expr(&mut self, expr: BoundExpr) -> BoundExpr {
         // TODO: support more data types.
-        match &expr.kind {
-            BoundExprKind::BinaryOp(binary_op) => match &binary_op.op {
-                BinaryOperator::Plus => {
-                    match (&binary_op.left_expr.kind, &binary_op.right_expr.kind) {
-                        (
-                            BoundExprKind::Constant(DataValue::Int32(0)),
-                            BoundExprKind::ColumnRef(col),
-                        )
-                        | (
-                            BoundExprKind::ColumnRef(col),
-                            BoundExprKind::Constant(DataValue::Int32(0)),
-                        ) => BoundExpr {
-                            kind: BoundExprKind::ColumnRef(col.clone()),
-                            return_type: Some(col.desc.datatype().clone()),
-                        },
-                        (
-                            BoundExprKind::Constant(DataValue::Float64(val)),
-                            BoundExprKind::ColumnRef(col),
-                        )
-                        | (
-                            BoundExprKind::ColumnRef(col),
-                            BoundExprKind::Constant(DataValue::Float64(val)),
-                        ) => {
-                            if *val == 0.0 {
-                                BoundExpr {
-                                    kind: BoundExprKind::ColumnRef(col.clone()),
-                                    return_type: Some(col.desc.datatype().clone()),
-                                }
-                            } else {
-                                expr
-                            }
-                        }
-                        _ => expr,
-                    }
-                }
-                BinaryOperator::Minus => {
-                    match (&binary_op.left_expr.kind, &binary_op.right_expr.kind) {
-                        (
-                            BoundExprKind::ColumnRef(col),
-                            BoundExprKind::Constant(DataValue::Int32(0)),
-                        ) => BoundExpr {
-                            kind: BoundExprKind::ColumnRef(col.clone()),
-                            return_type: Some(col.desc.datatype().clone()),
-                        },
-                        (
-                            BoundExprKind::ColumnRef(col),
-                            BoundExprKind::Constant(DataValue::Float64(val)),
-                        ) => {
-                            if *val == 0.0 {
-                                BoundExpr {
-                                    kind: BoundExprKind::ColumnRef(col.clone()),
-                                    return_type: Some(col.desc.datatype().clone()),
-                                }
-                            } else {
-                                expr
-                            }
-                        }
-                        _ => expr,
-                    }
-                }
-                BinaryOperator::Multiply => {
-                    match (&binary_op.left_expr.kind, &binary_op.right_expr.kind) {
-                        (
-                            BoundExprKind::Constant(DataValue::Int32(1)),
-                            BoundExprKind::ColumnRef(col),
-                        )
-                        | (
-                            BoundExprKind::ColumnRef(col),
-                            BoundExprKind::Constant(DataValue::Int32(1)),
-                        ) => BoundExpr {
-                            kind: BoundExprKind::ColumnRef(col.clone()),
-                            return_type: Some(col.desc.datatype().clone()),
-                        },
-                        (
-                            BoundExprKind::Constant(DataValue::Float64(val)),
-                            BoundExprKind::ColumnRef(col),
-                        )
-                        | (
-                            BoundExprKind::ColumnRef(col),
-                            BoundExprKind::Constant(DataValue::Float64(val)),
-                        ) => {
-                            if *val == 1.0 {
-                                BoundExpr {
-                                    kind: BoundExprKind::ColumnRef(col.clone()),
-                                    return_type: Some(col.desc.datatype().clone()),
-                                }
-                            } else {
-                                expr
-                            }
-                        }
-                        _ => expr,
-                    }
-                }
-                BinaryOperator::Divide => {
-                    match (&binary_op.left_expr.kind, &binary_op.right_expr.kind) {
-                        (
-                            BoundExprKind::ColumnRef(col),
-                            BoundExprKind::Constant(DataValue::Int32(1)),
-                        ) => BoundExpr {
-                            kind: BoundExprKind::ColumnRef(col.clone()),
-                            return_type: Some(col.desc.datatype().clone()),
-                        },
-                        (
-                            BoundExprKind::ColumnRef(col),
-                            BoundExprKind::Constant(DataValue::Float64(val)),
-                        ) => {
-                            if *val == 1.0 {
-                                BoundExpr {
-                                    kind: BoundExprKind::ColumnRef(col.clone()),
-                                    return_type: Some(col.desc.datatype().clone()),
-                                }
-                            } else {
-                                expr
-                            }
-                        }
-                        _ => expr,
-                    }
-                }
-                _ => expr,
+        let new_kind = match &expr.kind {
+            BinaryOp(op) => match (&op.op, &op.left_expr.kind, &op.right_expr.kind) {
+                // x + 0, 0 + x
+                (Plus, Constant(Int32(0)), other) => other.clone(),
+                (Plus, other, Constant(Int32(0))) => other.clone(),
+                (Plus, Constant(Float64(f)), other) if *f == 0.0 => other.clone(),
+                (Plus, other, Constant(Float64(f))) if *f == 0.0 => other.clone(),
+                // x - 0
+                (Minus, other, Constant(Int32(0))) => other.clone(),
+                (Minus, other, Constant(Float64(f))) if *f == 0.0 => other.clone(),
+                // x * 0, 0 * x
+                (Multiply, Constant(Int32(0)), _) => Constant(Int32(0)),
+                (Multiply, _, Constant(Int32(0))) => Constant(Int32(0)),
+                (Multiply, Constant(Float64(f)), _) if *f == 0.0 => Constant(Float64(0.0)),
+                (Multiply, _, Constant(Float64(f))) if *f == 0.0 => Constant(Float64(0.0)),
+                // x * 1, 1 * x
+                (Multiply, Constant(Int32(1)), other) => other.clone(),
+                (Multiply, other, Constant(Int32(1))) => other.clone(),
+                (Multiply, Constant(Float64(f)), other) if *f == 1.0 => other.clone(),
+                (Multiply, other, Constant(Float64(f))) if *f == 1.0 => other.clone(),
+                // x / 1
+                (Divide, other, Constant(Int32(1))) => other.clone(),
+                (Divide, other, Constant(Float64(f))) if *f == 1.0 => other.clone(),
+
+                _ => expr.kind.clone(),
             },
-            _ => expr,
+            UnaryOp(op) => match (&op.op, &op.expr.kind) {
+                (UnaryOperator::Plus, other) => other.clone(),
+                _ => expr.kind.clone(),
+            },
+            TypeCast(op) => match (&op.ty, &op.expr.kind) {
+                (Ty::Boolean, k @ Constant(Bool(_))) => k.clone(),
+                (Ty::Int, k @ Constant(Int32(_))) => k.clone(),
+                (Ty::BigInt, k @ Constant(Int64(_))) => k.clone(),
+                (Ty::Double, k @ Constant(Float64(_))) => k.clone(),
+                (Ty::String, k @ Constant(String(_))) => k.clone(),
+                _ => expr.kind.clone(),
+            },
+            _ => expr.kind.clone(),
+        };
+        BoundExpr {
+            kind: new_kind,
+            return_type: expr.return_type,
         }
     }
 }
