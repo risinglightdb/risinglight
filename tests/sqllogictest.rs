0736d3679ee798ef25b24cdda48b8c9047059f99
Runji Wang <wangrunji0408@163.com>
2021-11-26 23:16:42 +0800

    feat: improve sqllogictest
    - show line on error
    - support subtest command
    - add code docs
    - no longer parse the input resultset. Instead, stringify the output chunks, then compare the strings.

--- tests/sqllogictest.rs
+++ tests/sqllogictest.rs
@@ -4,8 +4,7 @@
 
 use itertools::Itertools;
 use log::*;
-use risinglight::types::DataValue;
-use risinglight::{array::*, storage::SecondaryStorageOptions, Database};
+use risinglight::{array::*, storage::SecondaryStorageOptions, types::DataValue, Database};
 use std::path::Path;
 use tempfile::{tempdir, TempDir};
 use test_case::test_case;
@@ -80,47 +79,81 @@ fn init_logger() {
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Record {
+    /// A statement is an SQL command that is to be evaluated but from which we do not expect to
+    /// get results (other than success or failure).
     Statement {
+        line: u32,
         conditions: Vec<Condition>,
         /// The SQL command is expected to fail instead of to succeed.
         error: bool,
         /// The SQL command.
         sql: String,
     },
+    /// A query is an SQL command from which we expect to receive results. The result set might be
+    /// empty.
     Query {
+        line: u32,
         conditions: Vec<Condition>,
-        // type_string: String,
+        type_string: String,
         sort_mode: SortMode,
         label: Option<String>,
+        /// The SQL command.
         sql: String,
-        expected_results: Vec<ColumnValues>,
+        /// The expected results.
+        expected_results: Vec<String>,
     },
+    /// Subtest.
+    Subtest { name: String },
+    /// A halt record merely causes sqllogictest to ignore the rest of the test script.
+    /// For debugging use only.
     Halt,
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Condition {
+    /// The statement or query is skipped if an `onlyif` record for a different database engine is
+    /// seen.
     OnlyIf { db_name: String },
+    /// The statement or query is not evaluated if a `skipif` record for the target database engine
+    /// is seen in the prefix.
     SkipIf { db_name: String },
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum ColumnValues {
-    Int(Vec<Option<i32>>),
-    Float(Vec<Option<f64>>),
-    Text(Vec<Option<String>>),
-    Bool(Vec<Option<bool>>),
-}
-
-#[derive(Debug, PartialEq, Clone)]
 pub enum SortMode {
+    /// The default option. The results appear in exactly the order in which they were received
+    /// from the database engine.
     NoSort,
+    /// Gathers all output from the database engine then sorts it by rows.
     RowSort,
+    /// It works like rowsort except that it does not honor row groupings. Each individual result
+    /// value is sorted on its own.
     ValueSort,
 }
 
+/// The error type for parsing sqllogictest.
 #[derive(thiserror::Error, Debug, PartialEq, Clone)]
-pub enum ParseError {
+#[error("parse error at line {line}: {kind}")]
+pub struct Error {
+    kind: ErrorKind,
+    line: u32,
+}
+
+impl Error {
+    /// Returns the corresponding [`ErrorKind`] for this error.
+    pub fn kind(&self) -> ErrorKind {
+        self.kind.clone()
+    }
+
+    /// Returns the line number from which the error originated.
+    pub fn line(&self) -> u32 {
+        self.line
+    }
+}
+
+/// The error type for parsing sqllogictest.
+#[derive(thiserror::Error, Debug, PartialEq, Clone)]
+pub enum ErrorKind {
     #[error("unexpected token: {0:?}")]
     UnexpectedToken(String),
     #[error("unexpected EOF")]
@@ -131,33 +164,23 @@ pub enum ParseError {
     InvalidLine(String),
     #[error("invalid type string: {0:?}")]
     InvalidType(String),
-    #[error("value length mismatch: {0:?}")]
-    LengthMismatch(String),
-    #[error("{0}")]
-    ParseInt(#[from] std::num::ParseIntError),
-    #[error("{0}")]
-    ParseFloat(#[from] std::num::ParseFloatError),
-    #[error("{0}")]
-    ParseBool(#[from] std::str::ParseBoolError),
 }
 
-impl ColumnValues {
-    #[allow(dead_code)]
-    fn len(&self) -> usize {
-        match self {
-            ColumnValues::Int(c) => c.len(),
-            ColumnValues::Float(c) => c.len(),
-            ColumnValues::Text(c) => c.len(),
-            ColumnValues::Bool(c) => c.len(),
+impl ErrorKind {
+    fn at(self, line: usize) -> Error {
+        Error {
+            kind: self,
+            line: line as u32,
         }
     }
 }
 
-pub fn parse(script: &str) -> Result<Vec<Record>, ParseError> {
-    let mut lines = script.split('\n');
+/// Parse a sqllogictest script into a list of records.
+pub fn parse(script: &str) -> Result<Vec<Record>, Error> {
+    let mut lines = script.split('\n').enumerate();
     let mut records = vec![];
     let mut conditions = vec![];
-    while let Some(line) = lines.next() {
+    while let Some((num, line)) = lines.next() {
         if line.is_empty() || line.starts_with('#') {
             continue;
         }
@@ -167,6 +190,11 @@ pub fn parse(script: &str) -> Result<Vec<Record>, ParseError> {
             ["halt"] => {
                 records.push(Record::Halt);
                 break;
+            }
+            ["subtest", name] => {
+                records.push(Record::Subtest {
+                    name: name.to_string(),
+                });
             }
             ["skipif", db_name] => {
                 conditions.push(Condition::SkipIf {
@@ -179,47 +207,48 @@ pub fn parse(script: &str) -> Result<Vec<Record>, ParseError> {
                 });
             }
             &["statement", res] => {
+                let line = num as u32;
                 let error = match res {
                     "ok" => false,
                     "error" => true,
-                    _ => return Err(ParseError::UnexpectedToken(res.into())),
+                    _ => return Err(ErrorKind::UnexpectedToken(res.into()).at(num)),
                 };
-                let mut sql = lines.next().ok_or(ParseError::UnexpectedEOF)?.into();
-                for line in &mut lines {
+                let mut sql = lines
+                    .next()
+                    .ok_or_else(|| ErrorKind::UnexpectedEOF.at(num + 1))?
+                    .1
+                    .into();
+                for (_, line) in &mut lines {
                     if line.is_empty() {
                         break;
                     }
                     sql += line;
                 }
                 records.push(Record::Statement {
+                    line,
                     conditions: std::mem::take(&mut conditions),
                     error,
                     sql,
                 });
             }
             ["query", type_string, res @ ..] => {
-                let mut values = vec![];
-                for c in type_string.chars() {
-                    match c {
-                        'T' => values.push(ColumnValues::Text(vec![])),
-                        'I' => values.push(ColumnValues::Int(vec![])),
-                        'R' => values.push(ColumnValues::Float(vec![])),
-                        'B' => values.push(ColumnValues::Bool(vec![])),
-                        _ => return Err(ParseError::InvalidType(type_string.to_string())),
-                    }
-                }
+                let line = num as u32;
                 let sort_mode = match res.get(0) {
                     None | Some(&"nosort") => SortMode::NoSort,
                     Some(&"rowsort") => SortMode::RowSort,
                     Some(&"valuesort") => SortMode::ValueSort,
-                    Some(mode) => return Err(ParseError::InvalidSortMode(mode.to_string())),
+                    Some(mode) => return Err(ErrorKind::InvalidSortMode(mode.to_string()).at(num)),
                 };
                 let label = res.get(1).map(|s| s.to_string());
                 // The SQL for the query is found on second an subsequent lines of the record
                 // up to first line of the form "----" or until the end of the record.
-                let mut sql = lines.next().ok_or(ParseError::UnexpectedEOF)?.into();
+                let mut sql = lines
+                    .next()
+                    .ok_or_else(|| ErrorKind::UnexpectedEOF.at(num + 1))?
+                    .1
+                    .into();
                 let mut has_result = false;
-                for line in &mut lines {
+                for (_, line) in &mut lines {
                     if line.is_empty() || line == "----" {
                         has_result = line == "----";
                         break;
@@ -227,115 +256,35 @@ pub fn parse(script: &str) -> Result<Vec<Record>, ParseError> {
                     sql += line;
                 }
                 // Lines following the "----" are expected results of the query, one value per line.
+                let mut expected_results = vec![];
                 if has_result {
-                    for line in &mut lines {
+                    for (_, line) in &mut lines {
                         if line.is_empty() {
                             break;
                         }
-                        if line.split_whitespace().count() != values.len() {
-                            return Err(ParseError::LengthMismatch(line.into()));
-                        }
-                        for (v, col) in line.split_whitespace().zip(values.iter_mut()) {
-                            match col {
-                                ColumnValues::Int(c) if v == "NULL" => c.push(None),
-                                ColumnValues::Float(c) if v == "NULL" => c.push(None),
-                                ColumnValues::Text(c) if v == "NULL" => c.push(None),
-                                ColumnValues::Text(c) if v == "(empty)" => c.push(Some("".into())),
-                                ColumnValues::Bool(c) if v == "NULL" => c.push(None),
-                                ColumnValues::Int(c) => c.push(Some(v.parse()?)),
-                                ColumnValues::Float(c) => c.push(Some(v.parse()?)),
-                                ColumnValues::Text(c) => c.push(Some(v.into())),
-                                ColumnValues::Bool(c) => c.push(Some(v.parse()?)),
-                            }
-                        }
+                        let normalized_line = line.split_ascii_whitespace().join(" ");
+                        expected_results.push(normalized_line);
                     }
                 }
                 records.push(Record::Query {
+                    line,
                     conditions: std::mem::take(&mut conditions),
+                    type_string: type_string.to_string(),
                     sort_mode,
                     label,
                     sql,
-                    expected_results: values,
+                    expected_results,
                 });
             }
-            _ => return Err(ParseError::InvalidLine(line.into())),
+            _ => return Err(ErrorKind::InvalidLine(line.into()).at(num)),
         }
     }
     Ok(records)
 }
 
-impl From<ColumnValues> for ArrayImpl {
-    fn from(col: ColumnValues) -> Self {
-        match col {
-            ColumnValues::Int(c) => c.into_iter().collect::<PrimitiveArray<i32>>().into(),
-            ColumnValues::Float(c) => c.into_iter().collect::<PrimitiveArray<f64>>().into(),
-            ColumnValues::Bool(c) => c.into_iter().collect::<PrimitiveArray<bool>>().into(),
-            ColumnValues::Text(c) => c
-                .iter()
-                .map(|o| o.as_ref().map(|s| s.as_str()))
-                .collect::<UTF8Array>()
-                .into(),
-        }
-    }
-}
-
 struct SqlLogicTester {
     db: Database,
     testdir: TempDir,
-}
-
-fn consolidate_datachunk(chunks: Vec<DataChunk>) -> DataChunk {
-    let mut builders = chunks[0]
-        .arrays()
-        .iter()
-        .map(ArrayBuilderImpl::from_type_of_array)
-        .collect_vec();
-    for chunk in chunks {
-        for (a, b) in chunk.arrays().iter().zip(builders.iter_mut()) {
-            b.append(a);
-        }
-    }
-    builders.into_iter().map(|b| b.finish()).collect()
-}
-
-#[derive(PartialEq, Eq)]
-struct CmpDataValue(DataValue);
-
-impl PartialOrd for CmpDataValue {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.0.partial_cmp(&other.0)
-    }
-}
-
-impl Ord for CmpDataValue {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other).unwrap()
-    }
-}
-
-fn sort_datachunk(chunk: DataChunk) -> DataChunk {
-    let mut rows = vec![];
-    for idx in 0..chunk.cardinality() {
-        rows.push(
-            chunk
-                .get_row_by_idx(idx)
-                .into_iter()
-                .map(CmpDataValue)
-                .collect_vec(),
-        );
-    }
-    rows.sort();
-    let mut builders = chunk
-        .arrays()
-        .iter()
-        .map(ArrayBuilderImpl::from_type_of_array)
-        .collect_vec();
-    for row in rows {
-        for (d, b) in row.iter().zip(builders.iter_mut()) {
-            b.push(&d.0);
-        }
-    }
-    builders.into_iter().map(|b| b.finish()).collect()
 }
 
 impl SqlLogicTester {
@@ -349,47 +298,80 @@ impl SqlLogicTester {
     pub async fn test(&mut self, record: Record) {
         info!("test: {:?}", record);
         match record {
-            Record::Statement { error, sql, .. } => {
+            Record::Statement {
+                error, sql, line, ..
+            } => {
                 let sql = self.replace_keywords(sql);
                 let ret = self.db.run(&sql).await;
                 match ret {
                     Ok(_) if error => panic!(
-                        "statement is expected to fail, but actually succeed: {:?}",
-                        sql
+                        "line {}: statement is expected to fail, but actually succeed: {:?}",
+                        line, sql
                     ),
-                    Err(e) if !error => panic!("statement failed: {}\n\tSQL: {:?}", e, sql),
+                    Err(e) if !error => {
+                        panic!("line {}: statement failed: {}\n\tSQL: {:?}", line, e, sql)
+                    }
                     _ => {}
                 }
             }
             Record::Query {
+                line,
                 sql,
-                expected_results,
+                mut expected_results,
                 sort_mode,
+                type_string,
                 ..
             } => {
                 let sql = self.replace_keywords(sql);
-                let output = self.db.run(&sql).await.expect("query failed");
-                let expected: DataChunk =
-                    expected_results.into_iter().map(ArrayImpl::from).collect();
-                if output.is_empty() && expected.cardinality() == 0 {
-                    return;
+                let chunks = self.db.run(&sql).await.expect("query failed");
+                // check types
+                for chunk in &chunks {
+                    if chunk.arrays().len() != type_string.len() {
+                        panic!(
+                            "line {}: column length mismatch: expected: {}, actual: {}",
+                            line,
+                            type_string.len(),
+                            chunk.arrays().len()
+                        );
+                    }
+                    for (array, type_char) in chunk.arrays().iter().zip(type_string.chars()) {
+                        match (type_char, array) {
+                            ('B', ArrayImpl::Bool(_)) => {}
+                            ('T', ArrayImpl::UTF8(_)) => {}
+                            ('I', ArrayImpl::Int32(_) | ArrayImpl::Int64(_)) => {}
+                            ('R', ArrayImpl::Float64(_)) => {}
+                            _ => panic!(
+                                "line {}: column type mismatch: expected: '{}'",
+                                line, type_char
+                            ),
+                        }
+                    }
                 }
-                let (output, expected) = match sort_mode {
-                    SortMode::NoSort => (consolidate_datachunk(output), expected),
-                    SortMode::RowSort => (
-                        sort_datachunk(consolidate_datachunk(output)),
-                        sort_datachunk(expected),
-                    ),
+                let mut output = chunks
+                    .iter()
+                    .map(datachunk_to_strings)
+                    .flatten()
+                    .collect_vec();
+                match sort_mode {
+                    SortMode::NoSort => {}
+                    SortMode::RowSort => {
+                        output.sort();
+                        expected_results.sort();
+                    }
                     SortMode::ValueSort => todo!(),
                 };
-                if output != expected {
+                if output != expected_results {
                     panic!(
-                        "query result mismatch:\nSQL:\n{}\n\nExpected:\n{}\nActual:\n{}",
-                        sql, expected, output
+                        "line {}: query result mismatch:\nSQL:\n{}\n\nExpected:\n{}\nActual:\n{}",
+                        line,
+                        sql,
+                        expected_results.join("\n"),
+                        output.join("\n")
                     );
                 }
             }
             Record::Halt => {}
+            Record::Subtest { .. } => {}
         }
     }
 
@@ -405,4 +387,29 @@ impl SqlLogicTester {
     fn replace_keywords(&self, sql: String) -> String {
         sql.replace("__TEST_DIR__", self.testdir.path().to_str().unwrap())
     }
+}
+
+fn datachunk_to_strings(chunk: &DataChunk) -> Vec<String> {
+    let mut lines = vec![];
+    for row in 0..chunk.cardinality() {
+        let mut line = String::new();
+        for (col, array) in chunk.arrays().iter().enumerate() {
+            use std::fmt::Write;
+            if col != 0 {
+                write!(line, " ").unwrap();
+            }
+            match array.get(row) {
+                DataValue::Null => write!(line, "NULL"),
+                DataValue::Bool(v) => write!(line, "{}", v),
+                DataValue::Int32(v) => write!(line, "{}", v),
+                DataValue::Int64(v) => write!(line, "{}", v),
+                DataValue::Float64(v) => write!(line, "{}", v),
+                DataValue::String(s) if s == "" => write!(line, "(empty)"),
+                DataValue::String(s) => write!(line, "{}", s),
+            }
+            .unwrap();
+        }
+        lines.push(line);
+    }
+    lines
 }
