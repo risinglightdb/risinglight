8f5187363f5d6118886756d17c2722502396713f
Runji Wang <wangrunji0408@163.com>
2021-12-24 12:30:51 +0800

    revert(tutorial): remove tutorial codes (#268)

--- code/01-05/src/binder/statement/mod.rs
+++
@@ -1,7 +0,0 @@
-use super::*;
-
-mod create_table;
-mod insert;
-
-pub use self::create_table::*;
-pub use self::insert::*;
--- code/01-07/src/binder/statement/mod.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-
-mod create_table;
-mod insert;
-mod select;
-
-pub use self::create_table::*;
-pub use self::insert::*;
-pub use self::select::*;
