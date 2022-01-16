b7a88413b5d1765cc6aeea00b3039e7712ff4f5d
st1page <1245835950@qq.com>
2022-01-17 02:00:28 +0800

    fix in rules

--- src/optimizer/rules/filter_scan_rule.rs
+++ src/optimizer/rules/filter_scan_rule.rs
@@ -1,5 +1,5 @@
 use super::*;
-use crate::optimizer::plan_nodes::{LogicalFilter, LogicalTableScan};
+use crate::optimizer::plan_nodes::{LogicalFilter, LogicalTableScan, PlanTreeNodeUnary};
 
 pub struct FilterScanRule {}
 
@@ -7,17 +7,17 @@ impl Rule for FilterScanRule {
     fn apply(&self, plan: PlanRef) -> Result<PlanRef, ()> {
         let filter = plan.downcast_rc::<LogicalFilter>().map_err(|_| ())?;
         let scan = filter
-            .child
+            .child()
             .clone()
             .downcast_rc::<LogicalTableScan>()
             .map_err(|_| ())?;
         Ok(Rc::new(LogicalTableScan {
-            table_ref_id: scan.table_ref_id,
-            column_ids: scan.column_ids.clone(),
-            column_descs: scan.column_descs.clone(),
-            with_row_handler: scan.with_row_handler,
-            is_sorted: scan.is_sorted,
-            expr: Some(filter.expr.clone()),
+            table_ref_id: scan.table_ref_id(),
+            column_ids: scan.column_ids().to_vec(),
+            column_descs: scan.column_descs().to_vec(),
+            with_row_handler: scan.with_row_handler(),
+            is_sorted: scan.is_sorted(),
+            expr: Some(filter.expr().clone()),
         }))
     }
 }
