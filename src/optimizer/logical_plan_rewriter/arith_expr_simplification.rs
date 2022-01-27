b1e1eaf30336a7b1b30c132c18ca10f6a28524fb
MingjiHan <mingjihan@umass.edu>
2022-01-27 18:09:06 -0500

    add optimizer test framework
    Signed-off-by: MingjiHan <mingjihan@umass.edu>

--- src/optimizer/logical_plan_rewriter/arith_expr_simplification.rs
+++ src/optimizer/logical_plan_rewriter/arith_expr_simplification.rs
@@ -91,3 +91,76 @@ impl PlanRewriter for ArithExprSimplificationRule {
         Arc::new(plan.clone_with_rewrite_expr(self))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use serde::Serialize;
+
+    use crate::Database;
+    #[tokio::main]
+    #[test]
+    async fn test_expr_simplification() {
+        let db = Database::new_in_memory();
+        let create_stmt = "create table t(a int)";
+        let sql0 = "select a + 0 from t";
+        // a + 0 should be converted to a
+        let _ = db.run(create_stmt).await;
+
+        let plans = db.generate_execution_plan(sql0).unwrap();
+        assert_eq!(plans.len(), 1);
+        let buf = Vec::new();
+        let formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
+        let mut ser = serde_json::Serializer::with_formatter(buf, formatter);
+        plans[0].serialize(&mut ser).unwrap();
+        let ser_str = String::from_utf8(ser.into_inner()).unwrap();
+        println!("{}", ser_str);
+        assert_eq!(
+            &ser_str,
+            "{
+    \"logical\": {
+        \"project_expressions\": [
+            {
+                \"InputRef\": {
+                    \"index\": 0,
+                    \"return_type\": {
+                        \"kind\": {
+                            \"Int\": null
+                        },
+                        \"physical_kind\": \"Int32\",
+                        \"nullable\": true
+                    }
+                }
+            }
+        ],
+        \"child\": {
+            \"logical\": {
+                \"table_ref_id\": {
+                    \"database_id\": 0,
+                    \"schema_id\": 0,
+                    \"table_id\": 0
+                },
+                \"column_ids\": [
+                    0
+                ],
+                \"column_descs\": [
+                    {
+                        \"datatype\": {
+                            \"kind\": {
+                                \"Int\": null
+                            },
+                            \"physical_kind\": \"Int32\",
+                            \"nullable\": true
+                        },
+                        \"is_primary\": false
+                    }
+                ],
+                \"with_row_handler\": false,
+                \"is_sorted\": false,
+                \"expr\": null
+            }
+        }
+    }
+}"
+        );
+    }
+}
