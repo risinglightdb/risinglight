ec7a245f3eed4653216e4388107de2b4fc4731e4
asuka <312856403@qq.com>
2022-02-02 23:27:01 +0800

    clippy
    Signed-off-by: asuka <312856403@qq.com>

--- src/storage/secondary/column.rs
+++ src/storage/secondary/column.rs
@@ -75,7 +75,7 @@ pub trait ColumnIterator<A: Array> {
     /// iterating, the returned value should be 0.
     fn fetch_hint(&self) -> usize;
 
-    /// Skip 'cnt' items for this column_iterator.
+    /// Skip 'cnt' items for this `column_iterator`.
     fn skip(&mut self, cnt: usize);
 }
 
