cf58f676b39ba0e776aa7a4d879c19c0ccf53fea
xyz <a997647204@gmail.com>
2022-02-17 11:32:46 +0800

    fix(cli): don't panic on bind error (#481)
    * bug(cli): don't panic on bind error
    
    Signed-off-by: xiaoyong-z <a997647204@gmail.com>
    
    * bug(cli): move check to binder
    
    Signed-off-by: xiaoyong-z <a997647204@gmail.com>

--- src/binder/mod.rs
+++ src/binder/mod.rs
@@ -55,6 +55,8 @@ pub enum BindError {
     AmbiguousColumn,
     #[error("invalid table name: {0:?}")]
     InvalidTableName(Vec<Ident>),
+    #[error("SQL not supported")]
+    NotSupportedTSQL,
     #[error("invalid SQL")]
     InvalidSQL,
     #[error("cannot cast {0:?} to {1:?}")]
@@ -119,7 +121,10 @@ impl Binder {
             Statement::Explain { statement, .. } => {
                 Ok(BoundStatement::Explain((self.bind(&*statement)?).into()))
             }
-            _ => todo!("bind statement"),
+            Statement::ShowVariable { .. }
+            | Statement::ShowCreate { .. }
+            | Statement::ShowColumns { .. } => Err(BindError::NotSupportedTSQL),
+            _ => Err(BindError::InvalidSQL),
         }
     }
 }
