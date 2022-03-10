ab025e1f5fbcf5a43841abfb66654277ad1ae38c
xyz <a997647204@gmail.com>
2022-03-10 16:38:35 +0800

    fix(executor): Char and Varchar's length limit not respected (#558)
    Signed-off-by: xiaoyong-z <a997647204@gmail.com>

--- src/executor/values.rs
+++ src/executor/values.rs
@@ -1,9 +1,11 @@
 // Copyright 2022 RisingLight Project Authors. Licensed under Apache-2.0.
 
+use itertools::izip;
+
 use super::*;
 use crate::array::{ArrayBuilderImpl, DataChunk};
 use crate::binder::BoundExpr;
-use crate::types::DataType;
+use crate::types::{DataType, DataTypeKind};
 
 /// The executor of `values`.
 pub struct ValuesExecutor {
@@ -17,17 +19,38 @@ impl ValuesExecutor {
     #[try_stream(boxed, ok = DataChunk, error = ExecutorError)]
     pub async fn execute(self) {
         for chunk in self.values.chunks(PROCESSING_WINDOW_SIZE) {
+            type Type = DataTypeKind;
             // Create array builders.
-            let mut builders = self
-                .column_types
+            let column_types = &self.column_types;
+            let mut builders = column_types
                 .iter()
                 .map(|ty| ArrayBuilderImpl::with_capacity(chunk.len(), ty))
                 .collect_vec();
             // Push value into the builder.
             let dummy = DataChunk::single(0);
             for row in chunk {
-                for (expr, builder) in row.iter().zip_eq(&mut builders) {
+                for (expr, column_type, builder) in izip!(row, column_types, &mut builders) {
                     let value = expr.eval(&dummy)?;
+                    let size = match column_type.kind {
+                        Type::Varchar(size) => size,
+                        Type::Char(size) => size,
+                        _ => None,
+                    };
+                    if let Some(width) = size {
+                        let item_length = if let DataValue::String(x) = &value.get(0) {
+                            x.len() as u64
+                        } else if let DataValue::Null = &value.get(0) {
+                            0
+                        } else {
+                            unreachable!()
+                        };
+                        if item_length > width {
+                            return Err(ExecutorError::ExceedLengthLimit {
+                                length: item_length,
+                                width,
+                            });
+                        }
+                    }
                     builder.push(&value.get(0));
                 }
             }
