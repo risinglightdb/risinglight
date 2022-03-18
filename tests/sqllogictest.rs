4b29fb11ca3d1b137604863297dba255bc4cfc0d
Alex Chi <iskyzh@gmail.com>
2022-03-18 20:47:45 +0800

    feat(ci): parallism test
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- tests/sqllogictest.rs
+++ tests/sqllogictest.rs
@@ -1,62 +1,85 @@
 // Copyright 2022 RisingLight Project Authors. Licensed under Apache-2.0.
 
 use std::path::Path;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::thread::available_parallelism;
 
+use futures::{Future, StreamExt};
 use risinglight::array::*;
 use risinglight::storage::SecondaryStorageOptions;
 use risinglight::{Database, Error};
 use tempfile::tempdir;
-use tokio::runtime::Runtime;
 
-#[test]
-fn run_all_test_files() {
+#[tokio::test(flavor = "multi_thread")]
+async fn run_all_test_files() {
     const PATTERN: &str = "tests/sql/**/[!_]*.slt"; // ignore files start with '_'
     const MEM_BLOCKLIST: &[&str] = &["statistics.slt"];
     const DISK_BLOCKLIST: &[&str] = &[];
 
     let paths = glob::glob(PATTERN).expect("failed to find test files");
-    let mut pass = true;
+
+    let mut test_cases: Vec<Pin<Box<dyn Future<Output = ()>>>> = vec![];
+
     for entry in paths {
         let path = entry.expect("failed to read glob entry");
-        let subpath = path.strip_prefix("tests/sql").unwrap().to_str().unwrap();
+        let subpath = path
+            .strip_prefix("tests/sql")
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string();
         if !MEM_BLOCKLIST.iter().any(|p| subpath.contains(p)) {
-            println!("-- running {} (mem) --", path.to_str().unwrap());
-            pass &= test_mem(subpath);
+            let path = path.clone();
+            let subpath = subpath.clone();
+            test_cases.push(Box::pin(async move {
+                println!("-- running {} (mem) --", path.to_str().unwrap());
+                test_mem(&subpath).await
+            }));
         }
         if !DISK_BLOCKLIST.iter().any(|p| subpath.contains(p)) {
-            println!("-- running {} (disk) --", path.to_str().unwrap());
-            pass &= test_disk(subpath);
+            let path = path.clone();
+            let subpath = subpath.clone();
+            test_cases.push(Box::pin(async move {
+                println!("-- running {} (disk) --", path.to_str().unwrap());
+                test_disk(&subpath).await
+            }));
         }
     }
-    assert!(pass);
+
+    let stream =
+        futures::stream::iter(test_cases).buffer_unordered(available_parallelism().unwrap().get());
+
+    stream.count().await;
 }
 
-fn test_mem(name: &str) -> bool {
+async fn test_mem(name: &str) {
     init_logger();
-    let mut tester = sqllogictest::Runner::new(DatabaseWrapper {
-        rt: Runtime::new().unwrap(),
-        db: Database::new_in_memory(),
-    });
+    let db = Arc::new(Database::new_in_memory());
+    let mut tester = sqllogictest::Runner::new(DatabaseWrapper { db: db.clone() });
     tester.enable_testdir();
     tester
-        .run_file(Path::new("tests/sql").join(name))
-        .map_err(|e| println!("{}", e))
-        .is_ok()
+        .run_file_async(Path::new("tests/sql").join(name))
+        .await
+        .unwrap();
+    db.shutdown().await.unwrap();
 }
 
-fn test_disk(name: &str) -> bool {
+async fn test_disk(name: &str) {
     init_logger();
     let temp_dir = tempdir().unwrap();
-    let rt = Runtime::new().unwrap();
-    let db = rt.block_on(Database::new_on_disk(
-        SecondaryStorageOptions::default_for_test(temp_dir.path().to_path_buf()),
-    ));
-    let mut tester = sqllogictest::Runner::new(DatabaseWrapper { rt, db });
+    let db = Database::new_on_disk(SecondaryStorageOptions::default_for_test(
+        temp_dir.path().to_path_buf(),
+    ))
+    .await;
+    let db = Arc::new(db);
+    let mut tester = sqllogictest::Runner::new(DatabaseWrapper { db: db.clone() });
     tester.enable_testdir();
     tester
-        .run_file(Path::new("tests/sql").join(name))
-        .map_err(|e| println!("{}", e))
-        .is_ok()
+        .run_file_async(Path::new("tests/sql").join(name))
+        .await
+        .unwrap();
+    db.shutdown().await.unwrap();
 }
 
 fn init_logger() {
@@ -66,24 +89,18 @@ fn init_logger() {
 }
 
 struct DatabaseWrapper {
-    rt: Runtime,
-    db: Database,
+    db: Arc<Database>,
 }
 
-impl sqllogictest::DB for DatabaseWrapper {
+#[async_trait::async_trait]
+impl sqllogictest::AsyncDB for DatabaseWrapper {
     type Error = Error;
-    fn run(&mut self, sql: &str) -> Result<String, Self::Error> {
-        let chunks = self.rt.block_on(self.db.run(sql))?;
+    async fn run(&mut self, sql: &str) -> Result<String, Self::Error> {
+        let chunks = self.db.run(sql).await?;
         let output = chunks
             .iter()
             .map(datachunk_to_sqllogictest_string)
             .collect();
         Ok(output)
-    }
-}
-
-impl Drop for DatabaseWrapper {
-    fn drop(&mut self) {
-        self.rt.block_on(self.db.shutdown()).unwrap();
     }
 }
