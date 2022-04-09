0b1624160403783c04257934f62a74fa2a05ac07
d2lark <lichengamoy@gmail.com>
2022-04-09 12:40:34 +0800

    adjust to merge rows with same key
    Signed-off-by: d2lark <lichengamoy@gmail.com>

--- src/executor/sort_merge_join.rs
+++ src/executor/sort_merge_join.rs
@@ -18,74 +18,80 @@ pub struct SortMergeJoinExecutor {
 impl SortMergeJoinExecutor {
     #[try_stream(boxed, ok = DataChunk, error = ExecutorError)]
     pub async fn execute(self) {
-        let mut left_rows = row_stream(self.left_child);
-        let mut right_rows = row_stream(self.right_child);
-        let mut left_row;
-        let mut right_row;
-        let first_left_row = left_rows.next().await;
-        let first_right_row = right_rows.next().await;
-        if first_left_row.is_none() || first_right_row.is_none() {
-            yield Err(ExecutorError::NotNullable)?
-        } else {
-            left_row = first_left_row.unwrap()?;
-            right_row = first_right_row.unwrap()?;
-            // build
-            let data_types = self.left_types.iter().chain(self.right_types.iter());
-            let mut builder = DataChunkBuilder::new(data_types, PROCESSING_WINDOW_SIZE);
+        let mut left_chunks = same_key_chunks(row_stream(self.left_child), self.left_column_index);
+        let mut right_chunks =
+            same_key_chunks(row_stream(self.right_child), self.right_column_index);
+        let mut left_current_chunk;
+        let mut right_current_chunk;
 
-            loop {
-                match compare_row(
-                    &left_row,
-                    &right_row,
-                    self.left_column_index,
-                    self.right_column_index,
-                ) {
-                    Ordering::Equal => {
-                        let values = left_row.into_iter().chain(right_row.clone().into_iter());
-                        if let Some(chunk) = builder.push_row(values) {
-                            yield chunk;
+        match (left_chunks.next().await, right_chunks.next().await) {
+            (Some(left_chunk), Some(right_chunk)) => {
+                left_current_chunk = left_chunk?;
+                right_current_chunk = right_chunk?;
+                // build
+                let data_types = self.left_types.iter().chain(self.right_types.iter());
+                let mut builder = DataChunkBuilder::new(data_types, PROCESSING_WINDOW_SIZE);
+
+                loop {
+                    match compare_row(
+                        &left_current_chunk[0],
+                        &right_current_chunk[0],
+                        self.left_column_index,
+                        self.right_column_index,
+                    ) {
+                        Ordering::Equal => {
+                            let full_join_chunk =
+                                full_join(&left_current_chunk, &right_current_chunk);
+                            for row in full_join_chunk {
+                                if let Some(chunk) = builder.push_row(row) {
+                                    yield chunk;
+                                }
+                            }
+
+                            if let Some(chunk) = left_chunks.next().await {
+                                left_current_chunk = chunk?;
+                            } else {
+                                break;
+                            }
                         }
-                        if let Some(row) = left_rows.next().await {
-                            left_row = row?;
-                        } else {
-                            break;
+                        Ordering::Greater => {
+                            if let Some(chunk) = right_chunks.next().await {
+                                right_current_chunk = chunk?;
+                            } else {
+                                break;
+                            }
                         }
-                    }
-                    Ordering::Greater => {
-                        if let Some(row) = right_rows.next().await {
-                            right_row = row?;
-                        } else {
-                            break;
-                        }
-                    }
-                    Ordering::Less => {
-                        if let Some(row) = left_rows.next().await {
-                            left_row = row?;
-                        } else {
-                            break;
+                        Ordering::Less => {
+                            if let Some(chunk) = left_chunks.next().await {
+                                left_current_chunk = chunk?;
+                            } else {
+                                break;
+                            }
                         }
                     }
                 }
+                // if rows line < PROCESSING_WINDOW_SIZE ,take rest rows out of builder
+                if let Some(chunk) = { builder }.take() {
+                    yield chunk;
+                }
             }
-            // if rows line < PROCESSING_WINDOW_SIZE ,take rest rows out of builder
-            if let Some(chunk) = { builder }.take() {
-                yield chunk;
-            }
+            _ => yield Err(ExecutorError::NotNullable)?,
         }
     }
 }
-
+// compare two rows by join key
 pub fn compare_row(
     left_row: &Row,
     right_row: &Row,
-    left_column_indexes: usize,
-    right_column_indexes: usize,
+    left_column_index: usize,
+    right_column_index: usize,
 ) -> Ordering {
-    let left_data_value = &left_row[left_column_indexes];
-    let right_data_value = &right_row[right_column_indexes];
+    let left_data_value = &left_row[left_column_index];
+    let right_data_value = &right_row[right_column_index];
     left_data_value.partial_cmp(right_data_value).unwrap()
 }
 
+// convert chunk stream to row stream
 #[try_stream(boxed, ok = Row, error = ExecutorError)]
 async fn row_stream(stream: BoxedExecutor) {
     #[for_await]
@@ -96,6 +102,72 @@ async fn row_stream(stream: BoxedExecutor) {
     }
 }
 
+// chunk same join key rows together
+#[try_stream(boxed, ok = Vec<Row>, error = ExecutorError)]
+async fn same_key_chunks(
+    row_stream: BoxStream<'static, Result<Row, ExecutorError>>,
+    column_index: usize,
+) {
+    let mut current_row = None;
+    let mut chunk = Vec::new();
+    #[for_await]
+    for row in row_stream {
+        match row {
+            Ok(row) => {
+                if current_row.is_none() {
+                    current_row = Some(row);
+                    chunk.push(current_row.clone().unwrap());
+                    continue;
+                }
+                if current_row.as_ref().unwrap().get(column_index) == row.get(column_index) {
+                    chunk.push(row);
+                } else {
+                    yield chunk;
+                    chunk = Vec::new();
+                    current_row = Some(row);
+                    chunk.push(current_row.clone().unwrap());
+                }
+            }
+            Err(e) => {
+                yield Err(e)?;
+            }
+        }
+    }
+    if !chunk.is_empty() {
+        yield chunk;
+    } else {
+        yield Err(ExecutorError::NotNullable)?;
+    }
+}
+
+// for example:
+// left same key rows: (join key = 0)
+// 2 c
+// 2 d
+
+// right same key rows: (join key = 0)
+// 2 e
+// 2 f
+
+// join_result
+// 2 c 2 e
+// 2 c 2 f
+// 2 d 2 e
+// 2 d 2 f
+fn full_join(left_chunk: &Vec<Row>, right_chunk: &Vec<Row>) -> Vec<Row> {
+    let mut join_chunk = Vec::new();
+    for left_row in left_chunk {
+        for right_row in right_chunk {
+            let values = left_row
+                .clone()
+                .into_iter()
+                .chain(right_row.clone().into_iter());
+            join_chunk.push(values.collect());
+        }
+    }
+    join_chunk
+}
+
 #[cfg(test)]
 mod tests {
     use test_case::test_case;
@@ -104,23 +176,22 @@ mod tests {
     use crate::array::ArrayImpl;
     use crate::types::{DataTypeExt, DataTypeKind};
     #[test_case(vec![1],vec![1],vec![1])]
-    #[test_case(vec![1],vec![1,2,3],vec![1])]
-    #[test_case(vec![1,2,3,4],vec![2,4,6],vec![2,4])]
+    #[test_case(vec![1],vec![1,1],vec![1,1])]
+    #[test_case(vec![1,1],vec![1,1],vec![1,1,1,1])]
+    #[test_case(vec![1,2,2,3,3],vec![2,3,3,4],vec![2,2,3,3,3,3])]
     #[tokio::test]
     async fn sort_merge_test(left_col: Vec<i32>, right_col: Vec<i32>, expected_col: Vec<i32>) {
         let left_child: BoxedExecutor = async_stream::try_stream! {
-                yield  [
-                ArrayImpl::new_int32(left_col.clone().into_iter().collect()),
-                ArrayImpl::new_int32(left_col.into_iter().collect())
+                yield  vec![
+                ArrayImpl::new_int32(left_col.into_iter().collect());2
             ]
             .into_iter()
             .collect()
         }
         .boxed();
         let right_child: BoxedExecutor = async_stream::try_stream! {
-                yield  [
-                    ArrayImpl::new_int32(right_col.clone().into_iter().collect()),
-                    ArrayImpl::new_int32(right_col.into_iter().collect())
+                yield  vec![
+                    ArrayImpl::new_int32(right_col.into_iter().collect());2
             ]
             .into_iter()
             .collect()
