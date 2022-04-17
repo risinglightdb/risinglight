8546d0afb8b7ff5c2545012baf8e3c50fd9e7262
Shmiwy <wyf000219@126.com>
2022-04-17 16:50:07 +0800

    fix: make header correct when using group by (#624)
    * executor: use DataChunkbuilder to refactor ValuesExecutor (#623)
    
    * use DataChunkbuilder to refactor ValuesExecutor
    Signed-off-by: Jayice <1185430411@qq.com>
    
    * code format
    Signed-off-by: Jayice <1185430411@qq.com>
    
    * code style
    Signed-off-by: Jayice <1185430411@qq.com>
    Signed-off-by: Shmiwy <wyf000219@126.com>
    
    * fix: make header correct when using group by(#538)
    
    Signed-off-by: Shmiwy <wyf000219@126.com>
    
    * fix: map group_keys to ColumnDesc
    
    Signed-off-by: Shmiwy wyf000219@gamil.com
    Signed-off-by: Shmiwy <wyf000219@126.com>
    
    * fix: use expr's return type instead of child schema's
    
    Signed-off-by: Shmiwy wyf000219@gamil.com
    Signed-off-by: Shmiwy <wyf000219@126.com>
    
    * build: bump toolchain and deps, remove unused dep (#629)
    
    Signed-off-by: TennyZhuang <zty0826@gmail.com>
    Signed-off-by: Shmiwy <wyf000219@126.com>
    
    Co-authored-by: JAYICE <49588871+JayiceZ@users.noreply.github.com>
    Co-authored-by: TennyZhuang <zty0826@gmail.com>

--- src/optimizer/plan_nodes/logical_aggregate.rs
+++ src/optimizer/plan_nodes/logical_aggregate.rs
@@ -69,13 +69,15 @@ impl_plan_tree_node_for_unary!(LogicalAggregate);
 impl PlanNode for LogicalAggregate {
     fn schema(&self) -> Vec<ColumnDesc> {
         let child_schema = self.child.schema();
-        let mut input_refs = vec![];
         self.group_keys
             .iter()
-            .for_each(|expr| expr.resolve_input_ref(&mut input_refs));
-        input_refs
-            .iter()
-            .map(|expr| child_schema[expr.index].clone())
+            .map(|expr| {
+                ColumnDesc::new(
+                    expr.return_type().unwrap(),
+                    expr.format_name(&child_schema),
+                    false,
+                )
+            })
             .chain(self.agg_calls.iter().map(|agg_call| {
                 agg_call
                     .return_type
