a5644b13621063b4f38f45155defaf8157498739
Frank Ma <fedomn.ma@gmail.com>
2022-04-18 11:25:05 +0800

    refactor(planner): use AggInputRefResolver to handle group by expr (#630)
    Signed-off-by: Fedomn <fedomn.ma@gmail.com>

--- src/optimizer/logical_plan_rewriter/input_ref_resolver.rs
+++ src/optimizer/logical_plan_rewriter/input_ref_resolver.rs
@@ -86,7 +86,25 @@ impl PlanRewriter for InputRefResolver {
             .iter()
             .map(|e| Some(e.clone()))
             .collect();
-        let ret = Arc::new(proj.clone_with_rewrite_expr(new_child, self));
+
+        let ret = match new_child.node_type() {
+            PlanNodeType::LogicalAggregate => {
+                let group_keys = self
+                    .bindings
+                    .iter()
+                    .map(|it| it.clone().unwrap())
+                    .collect::<Vec<_>>();
+                let mut resolver = AggInputRefResolver::new(group_keys.len());
+                let mut select_list = proj.project_expressions().to_vec();
+                for expr in &mut select_list {
+                    resolver.resolve_select_expr(expr, &group_keys);
+                }
+                let new_proj = LogicalProjection::new(select_list, new_child.clone());
+                Arc::new(new_proj.clone_with_rewrite_expr(new_child, self))
+            }
+            _ => Arc::new(proj.clone_with_rewrite_expr(new_child, self)),
+        };
+
         self.bindings = bindings;
         ret
     }
@@ -108,5 +126,179 @@ impl PlanRewriter for InputRefResolver {
     }
     fn rewrite_logical_values(&mut self, plan: &LogicalValues) -> PlanRef {
         Arc::new(plan.clone_with_rewrite_expr(self))
+    }
+}
+
+/// Resolves select expression into `InputRef` using group by expressions
+/// for parent node of `LogicalAggregate`.
+#[derive(Default)]
+struct AggInputRefResolver {
+    agg_start_index: usize,
+}
+
+impl AggInputRefResolver {
+    fn new(group_key_count: usize) -> Self {
+        AggInputRefResolver {
+            agg_start_index: group_key_count,
+        }
+    }
+
+    /// using group by exprs to resolve select expr into `InputRef` which include two cases:
+    /// 1. found identical select expr in group by exprs and replace it with `InputRef`
+    /// 2. found aggregate function in select expr and replace it with `InputRef`
+    fn resolve_select_expr(&mut self, expr: &mut BoundExpr, group_keys: &Vec<BoundExpr>) {
+        use BoundExpr::*;
+
+        // if found identical select expr in group by exprs, replace select expr with `InputRef`
+        if let Some(i) = group_keys.iter().position(|e| e == expr) {
+            *expr = InputRef(BoundInputRef {
+                index: i,
+                return_type: expr.return_type().unwrap(),
+            });
+            return;
+        }
+
+        match expr {
+            // due to aggregate exprs are behind group by exprs, so we used group by exprs count as
+            // InputRef's based index
+            AggCall(agg) => {
+                *expr = InputRef(BoundInputRef {
+                    index: self.agg_start_index,
+                    return_type: agg.return_type.clone(),
+                });
+                self.agg_start_index += 1;
+            }
+            BinaryOp(bin_op) => {
+                self.resolve_select_expr(&mut bin_op.left_expr, group_keys);
+                self.resolve_select_expr(&mut bin_op.right_expr, group_keys);
+            }
+            UnaryOp(unary_op) => self.resolve_select_expr(&mut unary_op.expr, group_keys),
+            TypeCast(type_cast) => self.resolve_select_expr(&mut type_cast.expr, group_keys),
+            ExprWithAlias(expr_with_alias) => {
+                self.resolve_select_expr(&mut expr_with_alias.expr, group_keys)
+            }
+            IsNull(isnull) => self.resolve_select_expr(&mut isnull.expr, group_keys),
+            Constant(_) | ColumnRef(_) | InputRef(_) | Alias(_) => {}
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use sqlparser::ast::BinaryOperator;
+
+    use super::*;
+    use crate::binder::AggKind;
+    use crate::types::{DataTypeExt, DataTypeKind, DataValue};
+
+    #[test]
+    /// To be resolved SQL:
+    /// ```sql
+    /// select v2 + 1, sum(v1) from t group by v2 + 1
+    /// ```
+    /// After resolved select list: `[InputRef #0, InputRef #1]`
+    fn test_resolve_select_expr() {
+        let sum_v1_call = BoundExpr::AggCall(BoundAggCall {
+            kind: AggKind::Sum,
+            args: vec![BoundExpr::ColumnRef(BoundColumnRef {
+                column_ref_id: ColumnRefId::new(0, 0, 0, 0),
+                is_primary_key: false,
+                desc: DataTypeKind::Int(None).not_null().to_column("v1".into()),
+            })],
+            return_type: DataTypeKind::Int(None).not_null(),
+        });
+        let v2_plus_1_expr = BoundExpr::BinaryOp(BoundBinaryOp {
+            op: BinaryOperator::Plus,
+            left_expr: BoundExpr::ColumnRef(BoundColumnRef {
+                column_ref_id: ColumnRefId::new(0, 0, 0, 1),
+                is_primary_key: false,
+                desc: DataTypeKind::Int(None).not_null().to_column("v2".into()),
+            })
+            .into(),
+            right_expr: BoundExpr::Constant(DataValue::Int32(1)).into(),
+            return_type: Some(DataTypeKind::Int(None).not_null()),
+        });
+        let group_keys = vec![v2_plus_1_expr.clone()];
+        let mut select_list = vec![v2_plus_1_expr, sum_v1_call];
+
+        let mut resolver = AggInputRefResolver::new(group_keys.len());
+        for expr in &mut select_list {
+            resolver.resolve_select_expr(expr, &group_keys);
+        }
+
+        assert_eq!(
+            select_list[0],
+            BoundExpr::InputRef(BoundInputRef {
+                index: 0,
+                return_type: DataTypeKind::Int(None).not_null(),
+            })
+        );
+        assert_eq!(
+            select_list[1],
+            BoundExpr::InputRef(BoundInputRef {
+                index: 1,
+                return_type: DataTypeKind::Int(None).not_null(),
+            })
+        );
+    }
+
+    #[test]
+    /// To be resolved SQL:
+    /// ```sql
+    /// select v2 + 1 + sum(v1) from t group by v2 + 1
+    /// ```
+    /// After resolved select list: `[Plus(InputRef #0, InputRef #1)]`
+    fn test_resolve_select_expr_plus_agg_call() {
+        let sum_v1_call = BoundExpr::AggCall(BoundAggCall {
+            kind: AggKind::Sum,
+            args: vec![BoundExpr::ColumnRef(BoundColumnRef {
+                column_ref_id: ColumnRefId::new(0, 0, 0, 0),
+                is_primary_key: false,
+                desc: DataTypeKind::Int(None).not_null().to_column("v1".into()),
+            })],
+            return_type: DataTypeKind::Int(None).not_null(),
+        });
+        let v2_expr = BoundExpr::ColumnRef(BoundColumnRef {
+            column_ref_id: ColumnRefId::new(0, 0, 0, 1),
+            is_primary_key: false,
+            desc: DataTypeKind::Int(None).not_null().to_column("v2".into()),
+        });
+        let v2_plus_1_expr = BoundExpr::BinaryOp(BoundBinaryOp {
+            op: BinaryOperator::Plus,
+            left_expr: v2_expr.into(),
+            right_expr: BoundExpr::Constant(DataValue::Int32(1)).into(),
+            return_type: Some(DataTypeKind::Int(None).not_null()),
+        });
+        let v2_plus_1_plus_sum_expr = BoundExpr::BinaryOp(BoundBinaryOp {
+            op: BinaryOperator::Plus,
+            left_expr: v2_plus_1_expr.clone().into(),
+            right_expr: sum_v1_call.into(),
+            return_type: Some(DataTypeKind::Int(None).not_null()),
+        });
+        let group_keys = vec![v2_plus_1_expr];
+        let mut select_list = vec![v2_plus_1_plus_sum_expr];
+
+        let mut resolver = AggInputRefResolver::new(group_keys.len());
+        for expr in &mut select_list {
+            resolver.resolve_select_expr(expr, &group_keys);
+        }
+
+        assert_eq!(
+            select_list[0],
+            BoundExpr::BinaryOp(BoundBinaryOp {
+                op: BinaryOperator::Plus,
+                left_expr: BoundExpr::InputRef(BoundInputRef {
+                    index: 0,
+                    return_type: DataTypeKind::Int(None).not_null(),
+                })
+                .into(),
+                right_expr: BoundExpr::InputRef(BoundInputRef {
+                    index: 1,
+                    return_type: DataTypeKind::Int(None).not_null(),
+                })
+                .into(),
+                return_type: Some(DataTypeKind::Int(None).not_null()),
+            })
+        );
     }
 }
