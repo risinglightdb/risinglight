d28389a62069cee31882dcae4a4e10383e4518f2
Shmiwy <wyf000219@126.com>
2022-04-19 11:18:00 +0800

    feat(binder): support `primary key (a, b)` syntax (#631)
    Signed-off-by: Shmiwy <wyf000219@126.com>

--- src/catalog/table.rs
+++ src/catalog/table.rs
@@ -21,6 +21,7 @@ struct Inner {
     #[allow(dead_code)]
     is_materialized_view: bool,
     next_column_id: ColumnId,
+    primary_key_ids: Vec<ColumnId>,
 }
 
 impl TableCatalog {
@@ -38,12 +39,29 @@ impl TableCatalog {
                 columns: BTreeMap::new(),
                 is_materialized_view,
                 next_column_id: 0,
+                primary_key_ids: Vec::new(),
             }),
         };
+        let mut pk_ids = vec![];
         for col_catalog in columns {
+            if col_catalog.is_primary() {
+                pk_ids.push(col_catalog.id());
+            }
             table_catalog.add_column(col_catalog).unwrap();
         }
+
+        table_catalog.set_primary_key_ids(&pk_ids);
         table_catalog
+    }
+
+    pub fn set_primary_key_ids(&self, pk_ids: &[ColumnId]) {
+        let mut inner = self.inner.lock().unwrap();
+        inner.primary_key_ids = pk_ids.to_owned();
+    }
+
+    pub fn get_primary_key_ids(&self) -> Vec<ColumnId> {
+        let inner = self.inner.lock().unwrap();
+        inner.primary_key_ids.clone()
     }
 
     pub fn add_column(&self, col_catalog: ColumnCatalog) -> Result<ColumnId, CatalogError> {
@@ -128,5 +146,25 @@ mod tests {
         let col1_catalog = table_catalog.get_column_by_id(1).unwrap();
         assert_eq!(col1_catalog.name(), "b");
         assert_eq!(col1_catalog.datatype().kind(), DataTypeKind::Boolean);
+
+        // test with two primary key
+        let col0 = ColumnCatalog::new(
+            0,
+            DataTypeKind::Int(None)
+                .not_null()
+                .to_column_primary_key("a".into()),
+        );
+        let col1 = ColumnCatalog::new(
+            1,
+            DataTypeKind::Int(None)
+                .not_null()
+                .to_column_primary_key("b".into()),
+        );
+        let col2 = ColumnCatalog::new(2, DataTypeKind::Int(None).nullable().to_column("c".into()));
+        let col3 = ColumnCatalog::new(3, DataTypeKind::Int(None).nullable().to_column("d".into()));
+
+        let col_catalogs = vec![col0, col1, col2, col3];
+        let table_catalog = TableCatalog::new(0, "t".into(), col_catalogs, false);
+        assert_eq!(table_catalog.get_primary_key_ids(), vec![0, 1]);
     }
 }
