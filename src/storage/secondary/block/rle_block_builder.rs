306a6fa879bedccc09621d47538812ce6324c4de
xinchengforgit <2419743144@qq.com>
2022-06-21 16:35:55 +0800

    feat(storage) use variable size encoding to compress rle count
    Signed-off-by: xinchengforgit <2419743144@qq.com>

--- src/storage/secondary/block/rle_block_builder.rs
+++ src/storage/secondary/block/rle_block_builder.rs
@@ -2,17 +2,92 @@
 
 use std::borrow::Borrow;
 
-use bytes::BufMut;
+use bytes::{Buf, BufMut};
+use prost::DecodeError;
 use risinglight_proto::rowset::BlockStatistics;
 
 use super::BlockBuilder;
 use crate::array::Array;
+
+fn encode_32<B>(mut value: u32, buf: &mut B)
+where
+    B: BufMut,
+{
+    loop {
+        if value < 0x80 {
+            buf.put_u8(value as u8);
+            break;
+        } else {
+            buf.put_u8(((value & 0x7F) | 0x80) as u8);
+            value >>= 7;
+        }
+    }
+}
+
+fn decode_u32<B>(buf: &mut B) -> Result<Vec<u32>, DecodeError>
+where
+    B: Buf,
+{
+    // &mut
+    let mut bytes = buf.chunk();
+    let len = bytes.len();
+    if len == 0 {
+        return Err(DecodeError::new("invalid varint"));
+    }
+    let mut ret: Vec<u32> = Vec::new();
+    let mut tot = 0;
+    while tot < len {
+        let (value, advance) = decode_u32_slice(bytes)?;
+        ret.push(value);
+        bytes.advance(advance);
+        tot += advance;
+    }
+    Ok(ret)
+}
+
+// 使用inline加速, 注意一个mut 引用可以调用一个非mut引用的函数
+#[inline]
+fn decode_u32_slice(bytes: &[u8]) -> Result<(u32, usize), DecodeError> {
+    assert!(!bytes.is_empty());
+    let mut b: u8 = unsafe { *bytes.get_unchecked(0) };
+    // u32 最多是5位
+    let mut part0: u32 = u32::from(b);
+    if b < 0x80 {
+        return Ok((u32::from(part0), 1));
+    };
+    part0 -= 0x80;
+    b = unsafe { *bytes.get_unchecked(1) };
+    part0 += u32::from(b) << 7;
+    if b < 0x80 {
+        return Ok((u32::from(part0), 2));
+    };
+    part0 -= 0x80 << 7;
+    b = unsafe { *bytes.get_unchecked(2) };
+    part0 += u32::from(b) << 14;
+    if b < 0x80 {
+        return Ok((u32::from(part0), 3));
+    };
+    part0 -= 0x80 << 14;
+    b = unsafe { *bytes.get_unchecked(3) };
+    part0 += u32::from(b) << 21;
+    if b < 0x80 {
+        return Ok((u32::from(part0), 4));
+    };
+    part0 -= 0x80 << 21;
+    b = unsafe { *bytes.get_unchecked(4) };
+    if b < 0x0f {
+        // or it will overflow
+        return Ok((part0 + (u32::from(b) << 28), 5));
+    };
+    Err(DecodeError::new("invalid varint"))
+}
 
 /// Encodes fixed-width data into a block with run-length encoding. The layout is
 /// rle counts and data from other block builder
 /// ```plain
 /// | rle_counts_num (u32) | rle_count (u16) | rle_count | data | data | (may be bit) |
 /// ```
+// rle == run-length encoding
 pub struct RleBlockBuilder<A, B>
 where
     A: Array,
@@ -20,16 +95,19 @@ where
     A::Item: PartialEq,
 {
     block_builder: B,
-    rle_counts: Vec<u16>,
+    // 其实要求的就是把rle_count压缩
+    rle_counts: Vec<u32>,
+    // rle_counts
     previous_value: Option<<A::Item as ToOwned>::Owned>,
-    cur_count: u16,
+    // previous_value A::Item
+    cur_count: u32,
 }
 
 impl<A, B> RleBlockBuilder<A, B>
 where
     A: Array,
     B: BlockBuilder<A>,
-    A::Item: PartialEq,
+    A::Item: PartialEq, // 可以实现== 比较
 {
     pub fn new(block_builder: B) -> Self {
         Self {
@@ -55,7 +133,16 @@ where
             self.cur_count = 1;
             return;
         }
-        if item != self.previous_value.as_ref().map(|x| x.borrow()) || self.cur_count == u16::MAX {
+        // x.to_owned() ==> create owned data from borrowed data, usually by cloning
+        // as_ref() ==> converts from &Option<T> to Option<&T>
+
+        // 我们可以这样理解, rust里面的reference 相当于是一个指针，也是一个类型, 所以其实也可以
+        // 视作有所有权
+        // 注意rust里面有四个概念需要区分一下
+        // map因为调用的参数是self, 所以会消耗自己
+        // 使用as_ref可以在不消耗自己的
+        // u32::MAX
+        if item != self.previous_value.as_ref().map(|x| x.borrow()) || self.cur_count == u32::MAX {
             self.previous_value = item.map(|x| x.to_owned());
             self.block_builder.append(item);
             self.rle_counts.push(self.cur_count);
@@ -87,9 +174,13 @@ where
             return encoded_data;
         }
         self.rle_counts.push(self.cur_count);
+
+        // 几个关键点, as 关键字可以实现类型强转
         encoded_data.put_u32_le(self.rle_counts.len() as u32);
+
+        // 关键在于count这个地方可以改成把每一个count 通过variable encoding
         for count in self.rle_counts {
-            encoded_data.put_u16_le(count);
+            encode_32(count, &mut encoded_data);
         }
         let data = self.block_builder.finish();
         encoded_data.extend(data);
