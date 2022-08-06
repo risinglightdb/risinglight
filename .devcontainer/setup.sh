d5414f233abd7ad5af02d3a72371926e23354dee
Ben Ye <ben.ye@bytedance.com>
2022-08-06 10:46:44 -0700

    add github codespace devcontainer configuration
    Signed-off-by: Ben Ye <ben.ye@bytedance.com>

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,33 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  zsh \
+  vim \
+  build-essential \
+  openssl \
+  cmake \
+  protobuf-compiler
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
+rustup install nightly
+rustup component add rustfmt
+rustup component add rustfmt --toolchain nightly
+rustup component add clippy 
+rustup component add clippy --toolchain nightly
+
+cargo install cargo-expand
+cargo install cargo-edit
+cargo install cargo-nextest
+
+## setup and install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -R /root/.oh-my-zsh /home/$USERNAME
+cp /root/.zshrc /home/$USERNAME
+sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
