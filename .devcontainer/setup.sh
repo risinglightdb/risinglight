33c43110df0a521d5f8ea686118631fcf6370491
Ben Ye <ben.ye@bytedance.com>
2022-08-12 20:52:32 -0700

    address review comments
    Signed-off-by: Ben Ye <ben.ye@bytedance.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,7 +5,6 @@ apt-get install -y \
   git \
   gnupg2 \
   jq \
-  zsh \
   vim \
   build-essential \
   openssl \
@@ -15,19 +14,11 @@ apt-get install -y \
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
-rustup install nightly
-rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
-rustup component add clippy 
-rustup component add clippy --toolchain nightly
+toolchain=$(cat rust-toolchain)
+rustup install $toolchain
+rustup component add rustfmt --toolchain $toolchain
+rustup component add clippy --toolchain $toolchain
 
 cargo install cargo-expand
 cargo install cargo-edit
 cargo install cargo-nextest
-
-## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
