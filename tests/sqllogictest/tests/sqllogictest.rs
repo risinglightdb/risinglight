2542ae870fe3e4e391f5bd242e475b936cd6f6eb
xxchan <xxchan22f@gmail.com>
2022-08-21 22:44:24 +0300

    clipply
    Signed-off-by: xxchan <xxchan22f@gmail.com>

--- tests/sqllogictest/tests/sqllogictest.rs
+++ tests/sqllogictest/tests/sqllogictest.rs
@@ -27,14 +27,14 @@ fn main() {
             let path = path.clone();
             let engine = Engine::Mem;
             tests.push(Trial::test(
-                format!("{}::{}", engine, subpath.to_string()),
+                format!("{}::{}", engine, subpath),
                 move || Ok(build_runtime().block_on(test(&path, engine))?),
             ));
         }
         if !DISK_BLOCKLIST.iter().any(|p| subpath.contains(p)) {
             let engine = Engine::Disk;
             tests.push(Trial::test(
-                format!("{}::{}", engine, subpath.to_string()),
+                format!("{}::{}", engine, subpath),
                 move || Ok(build_runtime().block_on(test(&path, engine))?),
             ));
         }
