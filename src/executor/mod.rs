1358ab04a9bba4c98ff9ad40343726af76a6b2d8
lokax <m632656684@gmail.com>
2022-09-25 01:30:27 +0800

    feat(executor): support perfect hash aggregate (#690)
    Signed-off-by: lokax <m632656684@gmail.com>

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -38,6 +38,8 @@ use self::internal::*;
 use self::limit::*;
 use self::nested_loop_join::*;
 use self::order::*;
+#[allow(unused_imports)]
+use self::perfect_hash_agg::*;
 use self::projection::*;
 use self::simple_agg::*;
 #[allow(unused_imports)]
@@ -72,6 +74,7 @@ mod internal;
 mod limit;
 mod nested_loop_join;
 mod order;
+mod perfect_hash_agg;
 mod projection;
 mod simple_agg;
 mod sort_agg;
