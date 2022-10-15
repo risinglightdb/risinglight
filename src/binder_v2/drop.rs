87983d7fe0b0b0989032265e28917bf2a5142ded
Elias.Yao <siran0611@gmail.com>
2022-10-15 12:34:38 +0800

    feat(binder): redefine boundDrop parse and rename bindDrop to boundDrop
    Signed-off-by: Elias.Yao <siran0611@gmail.com>

--- src/binder_v2/drop.rs
+++ src/binder_v2/drop.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 use super::*;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Copy, Clone, Serialize, Deserialize)]
-pub struct BindDrop {
+pub struct BoundDrop {
     pub object: Object,
     pub if_exists: bool,
     pub cascade: bool,
@@ -17,7 +17,7 @@ pub enum Object {
     Table(TableRefId),
 }
 
-impl std::fmt::Display for BindDrop {
+impl std::fmt::Display for BoundDrop {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
@@ -35,57 +35,16 @@ impl std::fmt::Display for Object {
     }
 }
 
-#[derive(thiserror::Error, Debug, Clone)]
-#[error("parse drop id error: {}")]
-pub enum ParseDropError {
-    #[error("no leading '$'")]
-    NoLeadingDollar,
-    #[error("invalid table")]
-    InvalidTable,
-    #[error("invalid number: {0}")]
-    InvalidNum(#[from] std::num::ParseIntError),
-    #[error("invalid bool: {0}")]
-    InvalidBool(#[from] std::str::ParseBoolError),
-}
+impl FromStr for BoundDrop {
+    type Err = ();
 
-impl FromStr for BindDrop {
-    type Err = ParseDropError;
-
-    fn from_str(s: &str) -> RawResult<Self, Self::Err> {
-        let body = s.strip_prefix('$').ok_or(Self::Err::NoLeadingDollar)?;
-        let mut parts = body.rsplit('.');
-        let table_id = parts.next().ok_or(Self::Err::InvalidTable)?.parse()?;
-        let schema_id = parts.next().map_or(Ok(0), |s| s.parse())?;
-        let database_id = parts.next().map_or(Ok(0), |s| s.parse())?;
-        let if_exists = parts.next().map_or(Ok(false), |s| s.parse())?;
-        let cascade = parts.next().map_or(Ok(false), |s| s.parse())?;
-        Ok(BindDrop {
-            object: Object::Table(TableRefId {
-                database_id,
-                schema_id,
-                table_id,
-            }),
-            if_exists,
-            cascade,
-        })
+    fn from_str(_s: &str) -> RawResult<Self, Self::Err> {
+        Err(())
     }
 }
 
 impl Binder {
     pub(super) fn bind_drop(
-        &mut self,
-        object_type: ObjectType,
-        if_exists: bool,
-        names: Vec<ObjectName>,
-        cascade: bool,
-    ) -> Result {
-        self.push_context();
-        let ret = self.bind_drop_internal(object_type, if_exists, names, cascade);
-        self.pop_context();
-        ret
-    }
-
-    fn bind_drop_internal(
         &mut self,
         object_type: ObjectType,
         if_exists: bool,
@@ -101,13 +60,11 @@ impl Binder {
                     .get_table_id_by_name(database_name, schema_name, table_name)
                     .ok_or_else(|| BindError::InvalidTable(table_name.into()))?;
 
-                let drop = self.egraph.add(Node::BindDrop(BindDrop {
+                Ok(self.egraph.add(Node::BoundDrop(BoundDrop {
                     object: Object::Table(table_ref_id),
                     if_exists,
                     cascade,
-                }));
-
-                Ok(self.egraph.add(Node::Drop(drop)))
+                })))
             }
             _ => todo!(),
         }
