abfec31aeb3b94c7e0030ada55af77a4a3baada7
Runji Wang <wangrunji0408@163.com>
2022-11-10 18:48:56 +0800

    fix performance regression in HashJoin
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/executor_v2/hash_join.rs
+++ src/executor_v2/hash_join.rs
@@ -19,7 +19,7 @@ pub struct HashJoinExecutor {
     pub right_types: Vec<DataType>,
 }
 
-pub type JoinKeys = SmallVec<[DataValue; 16]>;
+pub type JoinKeys = SmallVec<[DataValue; 2]>;
 
 impl HashJoinExecutor {
     #[try_stream(boxed, ok = DataChunk, error = ExecutorError)]
@@ -34,13 +34,13 @@ impl HashJoinExecutor {
         .await?;
 
         // build
-        let mut hash_map: HashMap<JoinKeys, Vec<RowRef<'_>>> = HashMap::new();
+        let mut hash_map: HashMap<JoinKeys, SmallVec<[RowRef<'_>; 1]>> = HashMap::new();
         for chunk in &left_chunks {
             let keys_chunk = Evaluator::new(&self.left_keys).eval_list(chunk)?;
             for i in 0..chunk.cardinality() {
                 let keys = keys_chunk.row(i).values().collect();
                 let row = chunk.row(i);
-                hash_map.entry(keys).or_insert_with(Vec::new).push(row);
+                hash_map.entry(keys).or_insert_with(SmallVec::new).push(row);
                 tokio::task::consume_budget().await;
             }
         }
@@ -55,7 +55,9 @@ impl HashJoinExecutor {
             for i in 0..chunk.cardinality() {
                 let right_row = chunk.row(i);
                 let keys: JoinKeys = keys_chunk.row(i).values().collect();
-                right_keys.insert(keys.clone());
+                if matches!(self.op, Expr::LeftOuter | Expr::FullOuter) {
+                    right_keys.insert(keys.clone());
+                }
                 if let Some(left_rows) = hash_map.get(&keys) {
                     for left_row in left_rows {
                         let values = left_row.values().chain(right_row.values());
