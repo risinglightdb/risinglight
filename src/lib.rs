02e2dd4c1f25176899d7d4c988f4641e2c39b443
Mingji Han <mjhan@bu.edu>
2022-12-21 17:15:14 -0500

    change python compile configurations
    Signed-off-by: Mingji Han <mjhan@bu.edu>
    Signed-off-by: MingjiHan <mjhan@bu.edu>

--- src/lib.rs
+++ src/lib.rs
@@ -88,7 +88,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 }
 
 #[pymodule]
-fn risinglightdb(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+fn risinglight(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
