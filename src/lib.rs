907b9274b103c4b46209b2609265c5af34d4a8ea
MingjiHan <mjhan@bu.edu>
2022-12-21 14:54:51 -0500

    wtf
    Signed-off-by: MingjiHan <mjhan@bu.edu>

--- src/lib.rs
+++ src/lib.rs
@@ -78,3 +78,17 @@ pub use self::db::{Database, Error};
 #[cfg(feature = "jemalloc")]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
+
+/// Python Extension
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pymodule]
+fn risinglight(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
