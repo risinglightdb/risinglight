42117a70b3d7b121109f82305a7f1a1f14326297
ice1000 <ice1000kotlin@foxmail.com>
2023-01-28 00:47:24 +0000

    pretty: cleanup
    Signed-off-by: ice1000 <ice1000kotlin@foxmail.com>

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,18 +1,3 @@
 // Copyright 2022 RisingLight Project Authors. Licensed under Apache-2.0.
 
 pub mod time;
-
-pub mod pretty {
-    use std::borrow::Cow;
-    use std::collections::BTreeMap;
-
-    use pretty_xmlish::Pretty;
-
-    pub fn named_record<'a>(
-        name: impl Into<Cow<'a, str>>,
-        fields: BTreeMap<&'a str, Pretty<'a>>,
-        children: Vec<Pretty<'a>>,
-    ) -> Pretty<'a> {
-        Pretty::simple_record(name, fields, children)
-    }
-}
