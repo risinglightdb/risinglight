ab668e3e4c64e5cc5059002000dd3614864ba9c2
Runji Wang <wangrunji0408@163.com>
2023-05-02 18:31:08 +0800

    update planner tests
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- tests/planner_test/extract-common-predicate.planner.sql
+++ tests/planner_test/extract-common-predicate.planner.sql
@@ -2,9 +2,9 @@
 explain select * from t where (a = 1 and b = 2) or (a = 1 and c = 3)
 
 /*
-Filter                                                                                                       
+Filter
 ├── cond: and { lhs: = { lhs: a, rhs: 1 }, rhs: or { lhs: = { lhs: c, rhs: 3 }, rhs: = { lhs: b, rhs: 2 } } }
-├── cost: 4316                                                                                               
-└── Scan { table: t, list: [ a, b, c ], cost: 3000 }
+├── cost: 4316
+└──  Scan { table: t, list: [ a, b, c ], cost: 3000 }
 */
 
