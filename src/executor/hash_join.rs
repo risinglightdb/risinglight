f33995d9f53cd1eecb36247416a9880a54e4c42e
Runji Wang <wangrunji0408@163.com>
2023-08-05 17:47:54 +0800

    optimize hashjoin executor
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/executor/hash_join.rs
+++ src/executor/hash_join.rs
@@ -3,12 +3,11 @@
 use std::collections::{HashMap, HashSet};
 use std::vec::Vec;
 
-use futures::TryStreamExt;
 use smallvec::SmallVec;
 
 use super::*;
 use crate::array::{ArrayBuilderImpl, ArrayImpl, DataChunk, DataChunkBuilder, RowRef};
-use crate::types::{DataType, DataValue};
+use crate::types::{DataType, DataValue, Row};
 
 /// The executor for hash join
 pub struct HashJoinExecutor<const T: JoinType> {
@@ -32,43 +31,37 @@ pub type JoinKeys = SmallVec<[DataValue; 2]>;
 impl<const T: JoinType> HashJoinExecutor<T> {
     #[try_stream(boxed, ok = DataChunk, error = ExecutorError)]
     pub async fn execute(self, left: BoxedExecutor, right: BoxedExecutor) {
-        // collect all chunks from children
-        let (left_chunks, right_chunks) = async {
-            tokio::try_join!(
-                left.try_collect::<Vec<DataChunk>>(),
-                right.try_collect::<Vec<DataChunk>>(),
-            )
-        }
-        .await?;
-
         // build
-        let mut hash_map: HashMap<JoinKeys, SmallVec<[RowRef<'_>; 1]>> = HashMap::new();
-        for chunk in &left_chunks {
-            let keys_chunk = Evaluator::new(&self.left_keys).eval_list(chunk)?;
-            for i in 0..chunk.cardinality() {
-                let keys = keys_chunk.row(i).values().collect();
-                let row = chunk.row(i);
-                hash_map.entry(keys).or_insert_with(SmallVec::new).push(row);
-                tokio::task::consume_budget().await;
+        #[derive(Default, Debug)]
+        struct LeftKeyInfo {
+            rows: SmallVec<[Row; 1]>,
+            matched: bool,
+        }
+        let mut hash_map: HashMap<JoinKeys, LeftKeyInfo> = HashMap::new();
+        #[for_await]
+        for chunk in left {
+            let chunk = chunk?;
+            let keys_chunk = Evaluator::new(&self.left_keys).eval_list(&chunk)?;
+            for (row, keys) in chunk.rows().zip(keys_chunk.rows()) {
+                let keys = keys.values().collect();
+                hash_map.entry(keys).or_default().rows.push(row.to_owned());
             }
+            tokio::task::consume_budget().await;
         }
 
         let data_types = self.left_types.iter().chain(self.right_types.iter());
         let mut builder = DataChunkBuilder::new(data_types, PROCESSING_WINDOW_SIZE);
-        let mut right_keys = HashSet::new();
 
         // probe
-        for chunk in &right_chunks {
-            let keys_chunk = Evaluator::new(&self.right_keys).eval_list(chunk)?;
-            for i in 0..chunk.cardinality() {
-                let right_row = chunk.row(i);
-                let keys: JoinKeys = keys_chunk.row(i).values().collect();
-                if T == JoinType::LeftOuter || T == JoinType::FullOuter {
-                    right_keys.insert(keys.clone());
-                }
-                if let Some(left_rows) = hash_map.get(&keys) {
-                    for left_row in left_rows {
-                        let values = left_row.values().chain(right_row.values());
+        #[for_await]
+        for chunk in right {
+            let chunk = chunk?;
+            let keys_chunk = Evaluator::new(&self.right_keys).eval_list(&chunk)?;
+            for (right_row, keys) in chunk.rows().zip(keys_chunk.rows()) {
+                if let Some(left_rows) = hash_map.get_mut(&keys.values().collect::<JoinKeys>()) {
+                    left_rows.matched = true;
+                    for left_row in &left_rows.rows {
+                        let values = left_row.iter().cloned().chain(right_row.values());
                         if let Some(chunk) = builder.push_row(values) {
                             yield chunk;
                         }
@@ -81,28 +74,25 @@ impl<const T: JoinType> HashJoinExecutor<T> {
                         yield chunk;
                     }
                 }
-                tokio::task::consume_budget().await;
             }
+            tokio::task::consume_budget().await;
         }
 
         // append rows for left outer join
         if T == JoinType::LeftOuter || T == JoinType::FullOuter {
-            for chunk in &left_chunks {
-                let keys_chunk = Evaluator::new(&self.left_keys).eval_list(chunk)?;
-                for i in 0..chunk.cardinality() {
-                    let keys: JoinKeys = keys_chunk.row(i).values().collect();
-                    let row = chunk.row(i);
-                    if right_keys.contains(&keys) {
-                        continue;
-                    }
+            for (_, rows) in hash_map {
+                if rows.matched {
+                    continue;
+                }
+                for row in rows.rows {
                     // append row: (left, NULL)
                     let values =
-                        (row.values()).chain(self.right_types.iter().map(|_| DataValue::Null));
+                        (row.into_iter()).chain(self.right_types.iter().map(|_| DataValue::Null));
                     if let Some(chunk) = builder.push_row(values) {
                         yield chunk;
                     }
-                    tokio::task::consume_budget().await;
                 }
+                tokio::task::consume_budget().await;
             }
         }
 
