8ec9b632397fd6fc99f651c3f1f9d8c870a3af4a
Alex Chi <iskyzh@gmail.com>
2023-08-10 23:33:22 -0400

    more tests
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- tests/planner_test/storage-pushdown.planner.sql
+++ tests/planner_test/storage-pushdown.planner.sql
@@ -15,17 +15,43 @@ Scan { table: t1, list: [ a, b ], filter: null, cost: 0, rows: 0 }
 */
 
 -- use storage filter for primary key
-explain select * from t1 where a = 10;
+explain select * from t1 where a = 1;
 
 /*
-Scan { table: t1, list: [ a, b ], filter: = { lhs: a, rhs: 10 }, cost: 10, rows: 5 }
+Scan { table: t1, list: [ a, b ], filter: = { lhs: a, rhs: 1 }, cost: 10, rows: 5 }
 */
 
 -- use storage filter for a combination of primary key and other keys
-explain select * from t1 where a > 10 and b > 10;
+explain select * from t1 where a > 1 and a < 3 and b > 1;
 
 /*
-Filter { cond: and { lhs: > { lhs: b, rhs: 10 }, rhs: > { lhs: a, rhs: 10 } }, cost: 0, rows: 0 }
-└── Scan { table: t1, list: [ a, b ], filter: null, cost: 0, rows: 0 }
+Filter { cond: > { lhs: b, rhs: 1 }, cost: 16.05, rows: 2.5 }
+└── Scan
+    ├── table: t1
+    ├── list: [ a, b ]
+    ├── filter: and { lhs: > { lhs: 3, rhs: a }, rhs: > { lhs: a, rhs: 1 } }
+    ├── cost: 10
+    └── rows: 5
+*/
+
+-- use storage filter for a combination of primary key (always false) and other keys
+explain select * from t1 where a > 1 and a < 0 and b > 1;
+
+/*
+Filter { cond: > { lhs: b, rhs: 1 }, cost: 16.05, rows: 2.5 }
+└── Scan
+    ├── table: t1
+    ├── list: [ a, b ]
+    ├── filter: and { lhs: > { lhs: 0, rhs: a }, rhs: > { lhs: a, rhs: 1 } }
+    ├── cost: 10
+    └── rows: 5
+*/
+
+-- use storage filter for a combination of primary key (could be eliminated) and other keys
+explain select * from t1 where a > 1 and a > 3 and b > 1;
+
+/*
+Filter { cond: and { lhs: > { lhs: b, rhs: 1 }, rhs: > { lhs: a, rhs: 3 } }, cost: 15.1, rows: 1.25 }
+└── Scan { table: t1, list: [ a, b ], filter: > { lhs: a, rhs: 1 }, cost: 10, rows: 5 }
 */
 
