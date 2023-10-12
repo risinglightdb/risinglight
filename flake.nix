ea7a7e481b5e917ba4d24cbe818953973a215130
kosumi <chankocyo@gmail.com>
2023-10-12 10:40:47 -0600

    Add Nix shell
    Signed-off-by: kosumi <chankocyo@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Risinglight";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay,... }: let
+    lib = {
+      inherit (flake-utils.lib) defaultSystems eachSystem;
+    };
+    supportedSystems = [ "x86_64-linux" ];
+  in lib.eachSystem supportedSystems (system: let
+
+    pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (import rust-overlay)
+        ];
+      };
+    pinnedRust = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain).override {
+      extensions = ["rustc-dev" "rust-src" "rust-analyzer-preview" ];
+      targets = [ "x86_64-unknown-linux-gnu" ];
+    };
+    rustPlatform = pkgs.makeRustPlatform {
+      rustc = pinnedRust;
+      cargo = pinnedRust;
+    };
+  in {
+    
+devShell = pkgs.mkShell {
+  hardeningDisable = [
+    "fortify"
+  ];
+  nativeBuildInputs = [
+    pinnedRust
+    pkgs.protobuf
+  ];
+  buildInputs = [
+  ];
+
+  shellHook = ''
+  '';
+};
+
+  });
+}
