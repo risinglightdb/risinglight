1bfc14a5c698979ec906d788b140cfd1cdb03607
Runji Wang <wangrunji0408@163.com>
2024-03-11 00:17:58 +0800

    fix datagen
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/streaming/connector/datagen.rs
+++ src/streaming/connector/datagen.rs
@@ -28,29 +28,31 @@ pub fn build(schema: SchemaRef, options: &BTreeMap<String, String>) -> Result<De
         .map(|s| s.parse::<u32>().context("batch_size must be integer"))
         .transpose()?
         .unwrap_or(1000);
-    let number_of_rows = options
+    let mut number_of_rows = options
         .get("number_of_rows")
-        .map(|s| s.parse::<u32>().context("number_of_rows must be integer"))
+        .map(|s| s.parse::<usize>().context("number_of_rows must be integer"))
         .transpose()?
-        .unwrap_or(u32::MAX);
+        .unwrap_or(usize::MAX);
 
     let batch_interval = Duration::from_secs(1) * batch_size / rows_per_second;
     let mut generators: Vec<_> = schema
         .fields()
         .iter()
-        .map(|f| RandomGenerator::new(f.data_type(), batch_size as usize))
+        .map(|f| RandomGenerator::new(f.data_type()))
         .collect();
     let schema0 = schema.clone();
     Ok(async_stream::try_stream! {
         let mut next_output_instant = tokio::time::Instant::now();
-        for i in 0..number_of_rows {
+        while number_of_rows > 0 {
+            let num_rows = number_of_rows.min(batch_size as usize);
             let batch = RecordBatch::try_new(
                 schema.clone(),
                 generators.iter_mut()
-                    .map(|g| g.next().unwrap())
+                    .map(|g| g.next(num_rows))
                     .collect(),
             )?.into();
             next_output_instant += batch_interval;
+            number_of_rows -= num_rows;
             tokio::time::sleep_until(next_output_instant).await;
             yield batch;
         }
@@ -61,53 +63,47 @@ pub fn build(schema: SchemaRef, options: &BTreeMap<String, String>) -> Result<De
 struct RandomGenerator {
     data_type: DataType,
     rng: SmallRng,
-    batch_size: usize,
 }
 
 impl RandomGenerator {
     /// Create a new random generator for `data_type`.
-    ///
-    /// The generator will produce `batch_size` elements at a time.
-    fn new(data_type: &DataType, batch_size: usize) -> Self {
+    fn new(data_type: &DataType) -> Self {
         Self {
             data_type: data_type.clone(),
             rng: SmallRng::seed_from_u64(0),
-            batch_size,
         }
     }
 }
 
-impl Iterator for RandomGenerator {
-    type Item = ArrayRef;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        Some(match self.data_type {
+impl RandomGenerator {
+    /// Generate a new array with `num_rows` values.
+    fn next(&mut self, num_rows: usize) -> ArrayRef {
+        match self.data_type {
             DataType::Int16 => {
-                let mut builder = Int16Array::builder(self.batch_size);
-                for _ in 0..self.batch_size {
+                let mut builder = Int16Array::builder(num_rows);
+                for _ in 0..num_rows {
                     builder.append_value(self.rng.gen());
                 }
                 Arc::new(builder.finish())
             }
             DataType::Int32 => {
-                let mut builder = Int32Array::builder(self.batch_size);
-                for _ in 0..self.batch_size {
+                let mut builder = Int32Array::builder(num_rows);
+                for _ in 0..num_rows {
                     builder.append_value(self.rng.gen());
                 }
                 Arc::new(builder.finish())
             }
             DataType::Int64 => {
-                let mut builder = Int64Array::builder(self.batch_size);
-                for _ in 0..self.batch_size {
+                let mut builder = Int64Array::builder(num_rows);
+                for _ in 0..num_rows {
                     builder.append_value(self.rng.gen());
                 }
                 Arc::new(builder.finish())
             }
             DataType::Utf8 => {
-                let mut builder =
-                    StringBuilder::with_capacity(self.batch_size, self.batch_size * 10);
-                for _ in 0..self.batch_size {
-                    for b in (&mut self.rng).sample_iter(&Alphanumeric).take(10) {
+                let mut builder = StringBuilder::with_capacity(num_rows, num_rows * 100);
+                for _ in 0..num_rows {
+                    for b in (&mut self.rng).sample_iter(&Alphanumeric).take(100) {
                         use std::fmt::Write;
                         builder.write_char(b as char).unwrap();
                     }
@@ -116,6 +112,6 @@ impl Iterator for RandomGenerator {
                 Arc::new(builder.finish())
             }
             _ => todo!("random generator for {:?}", self.data_type),
-        })
+        }
     }
 }
