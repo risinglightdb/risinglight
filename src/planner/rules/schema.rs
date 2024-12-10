dc90a32817bfdb99fc8fac17918c149674bfa649
Runji Wang <wangrunji0408@163.com>
2024-12-10 23:17:53 +0800

    check column conflict in making schema
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/planner/rules/schema.rs
+++ src/planner/rules/schema.rs
@@ -10,34 +10,42 @@ pub type Schema = Vec<Id>;
 /// Returns the output expressions for plan node.
 pub fn analyze_schema(
     enode: &Expr,
-    x: impl Fn(&Id) -> Schema,
+    schema: impl Fn(&Id) -> Schema,
     node0: impl Fn(&Id) -> Expr,
 ) -> Schema {
     use Expr::*;
-    let concat = |v1: Vec<Id>, v2: Vec<Id>| v1.into_iter().chain(v2).collect();
+    let concat = |v1: Vec<Id>, v2: Vec<Id>| {
+        assert!(
+            v2.iter().all(|id| !v1.contains(id)),
+            "conflict columns for node: {enode:?}"
+        );
+        v1.into_iter().chain(v2).collect()
+    };
     match enode {
         // equal to child
-        Filter([_, c]) | Order([_, c]) | Limit([_, _, c]) | TopN([_, _, _, c]) | Empty(c) => x(c),
+        Filter([_, c]) | Order([_, c]) | Limit([_, _, c]) | TopN([_, _, _, c]) | Empty(c) => {
+            schema(c)
+        }
 
         // concat 2 children
         Join([t, _, l, r])
         | HashJoin([t, _, _, _, l, r])
         | MergeJoin([t, _, _, _, l, r])
         | Apply([t, l, r]) => match node0(t) {
-            Semi | Anti => x(l),
-            Mark => concat(x(l), vec![*t]), // child || Mark
-            _ => concat(x(l), x(r)),
+            Semi | Anti => schema(l),
+            Mark => concat(schema(l), vec![*t]), // child || Mark
+            _ => concat(schema(l), schema(r)),
         },
 
         // list is the source for the following nodes
         List(ids) => ids.to_vec(),
 
         // plans that change schema
-        Scan([_, columns, _]) => x(columns),
-        Values(vs) => x(&vs[0]),
-        Proj([exprs, _]) | Agg([exprs, _]) => x(exprs),
-        Window([exprs, child]) => concat(x(child), x(exprs)),
-        HashAgg([keys, aggs, _]) | SortAgg([keys, aggs, _]) => concat(x(keys), x(aggs)),
+        Scan([_, columns, _]) => schema(columns),
+        Values(vs) => schema(&vs[0]),
+        Proj([exprs, _]) | Agg([exprs, _]) => schema(exprs),
+        Window([exprs, child]) => concat(schema(child), schema(exprs)),
+        HashAgg([keys, aggs, _]) | SortAgg([keys, aggs, _]) => concat(schema(keys), schema(aggs)),
 
         // not plan node
         _ => vec![],
