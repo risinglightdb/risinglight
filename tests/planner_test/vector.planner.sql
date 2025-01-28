81220f43c784376ab839335731e46eb093a58188
Alex Chi <iskyzh@gmail.com>
2025-01-27 23:28:06 -0500

    update vector test
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- tests/planner_test/vector.planner.sql
+++ tests/planner_test/vector.planner.sql
@@ -2,7 +2,7 @@
 explain select * from t order by a <-> '[0, 0, 1]'::VECTOR(3);
 
 /*
-IndexScan { table: t, columns: [ a, b ], filter: true, op: <->, key: a, vector: [0,0,1], cost: 0, rows: 1 }
+IndexScan { table: t, columns: [ a, b ], filter: true, key: a, vector: [0,0,1], cost: 0, rows: 1 }
 */
 
 -- match the index
