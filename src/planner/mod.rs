f7a347fad9bafa73c1c6a29a9a60717b6e51a1f0
Alex Chi Z. <4198311+skyzh@users.noreply.github.com>
2025-01-27 23:18:14 -0500

    Update src/planner/mod.rs
    Co-authored-by: Runji Wang <wangrunji0408@163.com>
    Signed-off-by: Alex Chi Z. <4198311+skyzh@users.noreply.github.com>

--- src/planner/mod.rs
+++ src/planner/mod.rs
@@ -98,7 +98,7 @@ define_language! {
 
         // plans
         "scan" = Scan([Id; 3]),                 // (scan table [column..] filter)
-        "vector_index_scan" = IndexScan([Id; 6]), // (vector_index_scan table [column..] filter <op> key vector)
+        "index_scan" = IndexScan([Id; 6]), // (index_scan table [column..] filter <op> key value)
         "values" = Values(Box<[Id]>),           // (values [expr..]..)
         "proj" = Proj([Id; 2]),                 // (proj [expr..] child)
         "filter" = Filter([Id; 2]),             // (filter expr child)
